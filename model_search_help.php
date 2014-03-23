e7939922a9075e55c5d24d7494de90cec573168d
duncanfalk <djf@duncanfalk.com>
2014-03-23 15:38:19 -0400

    Formatting the Model Search Tips page

--- model_search_help.php
+++ model_search_help.php
@@ -1,32 +1,42 @@
 <?php include("includes/header.php"); ?>
-<table id="structure">
-	<tr>
-		<td id="navigation">
-			<a href="Search_Models_Menu.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Search Models</p></a>
-			<a href="index.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Main Page</p></a>	
-		</td>
-		<td id="page">
-			<h2>Tips on Searching Models </h2>
-			<p>1	Case doesn&#39t matter in your field entries. </p>
-<p>2	You&#39ll notice that each search criterion is in a horizontal bar. To get to a particular search option (e.g. &#39Name&#39), simply click on that title or bar and it will open up to give you an entry box. </p>
-<p>3	If there is a checkbox shown along with your search option (i.e. for categories with drop-down lists), always check it, as well as selecting from the drop-down list- if it&#39s not checked the site won&#39t search by that option, regardless of whether you&#39ve picked an item from the list. </p>
-<p>4	Under the ID section, you can only enter one type of ID for search. If you enter more than one, it will use only the one closer to the bottom for the search. </p>
-<p>5    If oyu search by a range of IDs- make sure the first ID number is less than the second, otherwise the search will fail. Also, If searching for multiple UMIDs, if you make the first number an 'LR' number and the second an 'SF', you will get a LARGE range of results!</p>
-<p>6	Under the General Model Information section though, you can choose one or multiple categories to search by (e.g. Vehicle type=car, AND Vehicle Make=Ford). This section always uses &#39AND&#39 logic- it will match only those models that meet all of the criteria entered. </p>
-<p>7	Note that all &#39generic&#39 models- those made up by Matchbox/Mattel are entered with a Vehicle Make of &#39Matchbox&#39 and Country of &#39US&#39.  </p>
-<p>8	Unreal models (i.e. half of the 2004 lineup or Batmobile) are listed with Vehicle Type of &#39Fantasy&#39. </p>
-<p>9	The &#39Text on the Model&#39 section will look for any models that have the entered text on their Body (i.e. as part of their design). It doesn&#39t include text on the base of the model.  </p>
-<p>10	If you select &#39Text on the Model&#39 and don&#39t get a match, try entering less text, or more prominent text. Don&#39t use punctuation. </p>
-<p>11	If you make selections in both the ID and General Model Info sections, it will search based on the General Model Info only. Similarly if you select &#39Text on the Model&#39, it will search only by that text. </p>
-<p>12	Once you&#39ve entered criteria, click on the &#39Search&#39 button to execute the search. </p>
-<p>13	Clicking on the Cancel/Clear button will clear out whatever has been entered/selected on the page, giving you a clean slate. </p>
-<p>14	Once you&#39ve run a search and are on the models page, you can return to the search page in 2 ways: </p>
-<p>	a.	Click on the &#39Return to Model Search&#39 menu option; this gives you a blank search page for your next search </p>
-<p>	b.	Click on the browser back button. This will leave whatever you last search info was on the search page, which may give you unexpected results if you don&#39t notice it. If you do use the back arrow, best to hit Cancel/Clear before entering your next search, unless you want to return to the same search criteria (i.e. if you searched on vehicle type and want to seaarch again on vehicle type, hitting back arrow will leave that box checked and you can choose your next type from the dropdown). </p>
-<p>15	If you are not getting the results you expect, please send me a comment and I will check the database- I could easily have made a typo! </p>
- </p>
 
-		</td>
-	</tr>
-</table>
+	<!-- Top Menu -->
+	<div class="row">
+		<div class="large-12 columns">
+			<a class="button" href="Search_Models_Menu.php">Return to Search Models</a>
+			<a class="button" href="index.php">Return to Main Page</a>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="large-12 columns">
+			<h2>Tips on Searching Models</h2>
+			<ol>
+				<li>Case doesn&#39;t matter in your field entries.</li>
+				<li>You&#39;ll notice that each search criterion is in a horizontal bar. To get to a particular search option (e.g. &#39;Name&#39;), simply click on that title or bar and it will open up to give you an entry box.</li>
+				<li>If there is a checkbox shown along with your search option (i.e. for categories with drop-down lists), always check it, as well as selecting from the drop-down list- if it&#39;s not checked the site won&#39;t search by that option, regardless of whether you&#39;ve picked an item from the list.</li>
+				<li>Under the ID section, you can only enter one type of ID for search. If you enter more than one, it will use only the one closer to the bottom for the search.</li>
+				<li>If you search by a range of IDs- make sure the first ID number is less than the second, otherwise the search will fail. Also, If searching for multiple UMIDs, if you make the first number an 'LR' number and the second an 'SF', you will get a LARGE range of results!</li>
+				<li>Under the General Model Information section though, you can choose one or multiple categories to search by (e.g. Vehicle type=car, AND Vehicle Make=Ford). This section always uses &#39;AND&#39; logic- it will match only those models that meet all of the criteria entered.</li>
+				<li>Note that all &#39;generic&#39; models- those made up by Matchbox/Mattel are entered with a Vehicle Make of &#39;Matchbox&#39; and Country of &#39;US&#39;.</li>
+				<li>Unreal models (i.e. half of the 2004 lineup or Batmobile) are listed with Vehicle Type of &#39;Fantasy&#39;.</li>
+				<li>The &#39;Text on the Model&#39; section will look for any models that have the entered text on their Body (i.e. as part of their design). It doesn&#39;t include text on the base of the model.</li>
+				<li>If you select &#39;Text on the Model&#39 and don&#39t get a match, try entering less text, or more prominent text. Don&#39;t use punctuation.</li>
+				<li>If you make selections in both the ID and General Model Info sections, it will search based on the General Model Info only. Similarly if you select &#39;Text on the Model&#39;, it will search only by that text.</li>
+				<li>Once you&#39;ve entered criteria, click on the &#39;Search&#39; button to execute the search.</li>
+				<li>Clicking on the Cancel/Clear button will clear out whatever has been entered/selected on the page, giving you a clean slate.</li>
+				<li>
+					Once you&#39;ve run a search and are on the models page, you can return to the search page in 2 ways:
+					<ol>
+						<li>Click on the &#39Return to Model Search&#39; menu option; this gives you a blank search page for your next search</li>
+						<li>Click on the browser back button. This will leave whatever you last search info was on the search page, which may give you unexpected results if you don&#39;t notice it. If you do use the back arrow, best to hit Cancel/Clear before entering your next search, unless you want to return to the same search criteria (i.e. if you searched on vehicle type and want to search again on vehicle type, hitting back arrow will leave that box checked and you can choose your next type from the dropdown).</li>
+					</ol>
+				</li>
+				<li>If you are not getting the results you expect, please send me a comment and I will check the database- I could easily have made a typo!</li>
+			</ol>
+		</div>
+	</div>
+
+ 
+ 
 <?php include("includes/footer.php"); ?>
