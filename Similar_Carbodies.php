b3cac0053a64c1a32e0f4672f2e4341aba7b0bd5
duanefalk <duanejfalk@yahoo.com>
2014-04-13 18:47:01 -0400

    changes to model diffs

--- Similar_Carbodies.php
+++ Similar_Carbodies.php
@@ -7,7 +7,7 @@
 		</td>
 		<td id="page">
 			<h2>Differences Between Superfast Carbodies Taxi Models</h2>
-			<p>The Carbodies Taxi FX 4 R was released in 1987 as MB174. A handful of models were made, mostly released in Britain, Australia or ROW. An updated version called the Carbodies London Taxi was issued in 2005 as MB667, with releases in the 1-75, Superfast and other series. Although no official blank was made, several Code 2 models were created against various of the standard versions.  </p>
+			<p>The Carbodies Taxi FX 4 R was released in 1987 as MB174 (UMID SF0322). A handful of models were made, mostly released in Britain, Australia or ROW. An updated version called the Carbodies London Taxi was issued in 2005 as MB667 (UMID SF0714), with releases in the 1-75, Superfast and other series. Although no official blank was made, several Code 2 models were created against various of the standard versions.  </p>
 			<p>The base information and casting are very different:</>
                         <p>1)  MB174 is called Taxi FX4R; MB667 is London taxi</p>
 			<p>2)  The base dates reflect their release years</p>
