b92bf3846e5ed927066c559336e7e30e29898255
duanefalk <duanejfalk@yahoo.com>
2014-04-20 14:36:31 -0400

    update to similar models

--- Similar_TBirds.php
+++ Similar_TBirds.php
@@ -18,12 +18,17 @@
  			<img src="Images/Similar_Models/MAN-212.jpg" width=340>
                         <img src="Images/Similar_Models/MAN-212-2.jpg" width=340>
                         <img src="Images/Similar_Models/MAN-212-3.jpg" width=340>
-                        <p></p>
+			<p></p>	
+			<p>NOTE: You may come across a version that has the grill and bumper casting of SF0368, but has chromed windows, and an unpainted metal base. This was a model Matchbox started to make in anticipation of a NASCAR set. They were unable to get the licensing though and never released the models. Somehow, many copies of the #28 Havoline and #3 Goodwrench versions they had produced made their way to the public in one of the factory moves. Be aware of these (photo of #28 below), but they are not listed as production models.</p>	
+                        <img src="Images/Similar_Models/MAN-212-4-1.jpg" width=200>
+			<img src="Images/Similar_Models/MAN-212-4-2.jpg" width=200>
+			<img src="Images/Similar_Models/MAN-212-4-3.jpg" width=200>
+			<p></p>
  
-                        <p>The second release was MAN# 268. There were, however, two toolings for this model- one in 1994 and one in 1995. There are casting differences so I/'ve broken them up into two UMID/'s /SF0697 for the /'94 and SF0899 for the /'95)</p>
-                        <p>UMID SF0397:</p>
+                        <p>The second release was MAN# 268. There were two toolings for this model- one in 1994 and one in 1995. There is a casting differences on the nose but I've left them together in UMID SF0397 (and noted in version attachments which have the cast grill and which don't).</p>
+                        <p>UMID SF0397 1994:</p>
                         <p>1)  The nose has only one cast grill panel- a rectangle in the center</p>
-                        <p>2)  No Thunderbird cast on the front bumpoer, thoguh it may be painted there</p>
+                        <p>2)  No Thunderbird cast on the front bumper, thoguh it may be painted there</p>
                         <p>3)  Still has a 'flat face' profile, no recessed light wells, but does have a groove cut across the nose at the level of the grill panel</p>
                         <p>4)  Rear bumper is smooth- no notch</p>
  			<img src="Images/Similar_Models/MAN-268-1-1.jpg" width=340>
@@ -31,7 +36,7 @@
                         <img src="Images/Similar_Models/MAN-268-1-3.jpg" width=340>			
 			<p></p>
 			
-                        <p>UMID SF0899:</p>
+                        <p>UMID SF0397 1995:</p>
                         <p>1)  The nose has no cast grill panel- it is completely smooth below the /'mail slot/'. Note that there may be grill panels painted onto the surface</p>
                         <p>2)  No Thunderbird cast on the front bumpoer, thoguh it may be painted there</p>
                         <p>3)  Still has a /'flat face/' profile, no recessed light wells</p>
@@ -39,7 +44,7 @@
  			<img src="Images/Similar_Models/MAN-268-2-1.jpg" width=340>
 			<p></p>
 			
-                        <p>The third Matchbox model was WRP02, released for the 1996-1998 NASCAR lines: </p>
+                        <p>The third Matchbox model was WRP02, released for the 1996-1998 NASCAR lines, under UMID SF0470: </p>
                         <p>1)  The nose of the car has a clear indent of the light wells- it's no longer a flat face.</p>
                         <p>2)  There is no cast grill. Grill openings may be painted on the nose, typically with two bullet shapes above rectangles</p>
                         <p>3)  Again, no Thunderbird cast on the front bumpoer, thoguh it may be painted there</p>
