d79c8bdd2a58f0aa1481aa8cf00b1c1f36725d5c
duanefalk <duanejfalk@yahoo.com>
2014-06-29 15:50:13 -0400

    full name

--- Authenticate-test.php
+++ Authenticate-test.php
@@ -51,6 +51,7 @@ $_SESSION['Sec_Lvl']=1;
 			                $_SESSION['Veh_Cond_Scheme']=$row["Veh_Cond_Scheme"];
 			                $_SESSION['Pkg_Cond_Scheme']=$row["Pkg_Cond_Scheme"];
 			                $_SESSION['Code2_Pref']=$row["Code2_Pref"];
+					$_SESSION['Full_name']=$row["User_name"];
 			                echo $userId.$password."<br />";
 			                echo $rows_count. ' '.$row[Username].' '.$row[Password]." ".$row[Sec_Lvl];
 			          
