16a4e49ae3c4e1804b38ca91183a52f026acfaa0
duanefalk <duanejfalk@yahoo.com>
2014-07-02 15:44:36 -0400

    updated about site

---
+++ About_site.php
@@ -0,0 +1,48 @@
+<?php include("includes/header.php"); ?>
+
+<div class="row">
+	<div class="large-12 columns">
+	
+		<h2>MBX-U Site FAQ's</h2>	
+			<ol>	
+				<li>What is MBX-U?</li>
+                                    <p>Matchbox University (MBX-U) is a non-commercial web site dedicated to the hobby of Matchbox collecting.</p>
+                                    <p>The goals for this site are:</p>
+                                    <ul>
+                                        <li>Provide basic information about Matchbox cars and the hobby</li>
+                                        <li>Provide a comprehensive database of Matchbox miniatures that can be searched easily and quickly and gives detailed information on each model.</li>
+                                        <li>I started the site as a way to catalog my own collection, then thought it might be useful for others as well. For those who create an account on the site, it provides a way to build and maintain an electronic record of your models, accessible from any web device.</li>
+                                        <li>Provide <i>up-to-date</i> links to other Matchbox web sites, collectors clubs and reliable sellers.</li>
+                                        <li>ive site users a mechanism to provide input on model information, corrections and upload info and photos to be included on the site.</li>
+                                    </ul>
+                                <li>Why is it called Matchbox University?</li>
+                                    <p>Many of the existing sites are called &#39;on-line databases&#39; or &#39;virtual museums&#39;, so I was looking for a different theme. Since first and foremost the site provides information the idea of a university occurred to me (complete with a Matchbox Professor!). So, like a university- there is a course catalog of &#39;lessons&#39;, a library of models (the database), and even a college store where you can see Code 2 models available (at no cost) for creating and account and other activities.</p>
+                                <li>What kind of information is available on the site?</li>
+                                    <p>The matchbox &#39;curriculum&#39; consists of a number of short write-ups on different topics pertinent to the hobby. It is geared more for newcomers to die-cast and Matchbox collecting, or for those who may have taken a hiatus, but recently returned to the hobby and need a quick refresher/catch-up.</p>
+                                <li>What's in the database?</li>
+                                    <p>There is a separate menu item that will take you to a page on the details of the MBX-U database approach and structure. Briefly though, MBX-U is built around a relational database rather than a text or excel listing. There are a variety of ways you can search for models or releases (how a model is packaged and sold). The database, for the foreseeable future, will be focused on Matchbox miniatures. Currently it covers the 1:64 vehicles such as those offered in the 1-120 series, 5-packs, etc. In the future I hope to expand to Skybusters and Convoys as well.</p>
+                                <li>Do I have to learn a new numbering method?</li>
+                                    <p>No! The database is structured around a unique model ID that I assigned each model, and a model/version/variation/release hierarchy. BUTÉ all of the usual methods of cataloging and organizing Matchbox models- Charlie Mack numbers, FAB/MAN#s, model names etc.- are all still there and you can use those. If you are interested in how the models are organized though, please check out &#39;About the Database&#39; from the &#39;About the Site&#39; menu.</p>
+                                <li>My own collection, really?</li>
+                                    <p>Indeed. You can create and name your own collection, as well as set some user-specific options. As you search for and drill down on specific model variations you can tag them to add to your collection, or add to a wish list. Once you have a collection, your model searches will show if you have that particular item or not, and of course you can print out lists of your collection or wish list models.</p>
+                                <li>I need an account?</li>
+                                    <p>Again, no. You can come to the site and do searches for models and releases or read the articles without creating an account or logging in. But, if you want to set some preferences on your searches, build and update your own collection, or provide feedback/input for the site, you will need to create an account. It&#39;s very simple, and of course no financial info is needed.</p>
+				<p>So&#133;</p>
+                                <p>Please look over the site and try out the functionality. My hope is that it provides newbies with some intro information on the hobby, and enthusiasts with some additional tools to help you better enjoy your collection.  And remember, this is all for fun! When someone asks me why I collect toys, or anything really, I tell them, for the pleasure of</p>					
+				<ul>
+                                    <li>Seeking: The excitement of filling in the holes in a collection, especially with that long sought-after model</li>
+                                    <li>Learning: Matchbox has a rich history and knowledge base. There&#39;s lots to learn, even after years in the hobby, and the knowing the backstory makes collecting the models so much better</li>
+                                    <li>Sharing: Matchbox collectors are great at passing along what they know about the hobby. Even the newest collector can find a new wheel variation at the store, or an unknown gem in an old collection. Tell us what you&#39;ve learned. </li>
+                                    <li>Enjoying! Make friends- join a club, go to collectors meets, and most of all have fun with those toys. Roll them, check them out from all angles, photograph them, move the moving parts. Remember- playing with them is what got you interested in the first place!! </li>
+                                </ul>
+			</ol>
+			
+			<li><a href="DB_explan.php">About the Database</a></li>
+			<li><a href="About_photos.php">About the Photos</a></li>
+			<li><a href="Ref_files.php">View Reference Files</a></li>
+			<li><a href="index.php">Return to Main Page</a></li>
+	
+	</div>
+</div>
+
+<?php include("includes/footer.php"); ?>
