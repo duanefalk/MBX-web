d6048d1b3c866fb227b8e402caf0a9f2044035bd
duncanfalk <djf@duncanfalk.com>
2014-07-17 08:45:38 -0700

    Formatting fixes

--- includes/header-home.php
+++ includes/header-home.php
@@ -1,32 +1,11 @@
 <!doctype html>
 <html class="no-js" lang="en">
 
-<head>
-	
+<head>	
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
 	<title>Matchbox University</title>
-	
-	<?php
-		// variety of color schemes bases on random number
-		//$color_choice = rand(1,4);
-		
-		//switch ($color_choice)
-			//{
-			//case 1:
-			//  echo '<link href="stylesheets/public1.css" media="all" rel="stylesheet" type="text/css" />';
-			//  break;
-			//case 2:
-			//  echo '<link href="stylesheets/public2.css" media="all" rel="stylesheet" type="text/css" />';
-			//  break;
-			//case 3:
-			//  echo '<link href="stylesheets/public4.css" media="all" rel="stylesheet" type="text/css" />';
-			//  break;
-			//default:
-			//  echo '<link href="stylesheets/public3.css" media="all" rel="stylesheet" type="text/css" />';
-			//}
-		?>
 		
 	<link href='http://fonts.googleapis.com/css?family=Lato:400,400italic,700' rel='stylesheet' type='text/css'>
 	<link href="stylesheets/foundation.css" media="all" rel="stylesheet" type="text/css" />
@@ -35,8 +14,6 @@
 	<link href="stylesheets/interior.css" media="all" rel="stylesheet" type="text/css" />
 	
 	<script src="js/vendor/modernizr.js"></script>
-	
-
 </head>
 
 <body class="home">
--- includes/header.php
+++ includes/header.php
@@ -2,31 +2,10 @@
 <html class="no-js" lang="en">
 
 <head>
-	
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	
 	<title>Matchbox University</title>
-	
-	<?php
-		// variety of color schemes bases on random number
-		//$color_choice = rand(1,4);
-		
-		//switch ($color_choice)
-			//{
-			//case 1:
-			//  echo '<link href="stylesheets/public1.css" media="all" rel="stylesheet" type="text/css" />';
-			//  break;
-			//case 2:
-			//  echo '<link href="stylesheets/public2.css" media="all" rel="stylesheet" type="text/css" />';
-			//  break;
-			//case 3:
-			//  echo '<link href="stylesheets/public4.css" media="all" rel="stylesheet" type="text/css" />';
-			//  break;
-			//default:
-			//  echo '<link href="stylesheets/public3.css" media="all" rel="stylesheet" type="text/css" />';
-			//}
-		?>
 		
 	<link href='http://fonts.googleapis.com/css?family=Lato:400,400italic,700' rel='stylesheet' type='text/css'>
 	<link href="stylesheets/foundation.css" media="all" rel="stylesheet" type="text/css" />
@@ -34,9 +13,7 @@
 	<link href="stylesheets/style.css" media="all" rel="stylesheet" type="text/css" />
 	<link href="stylesheets/interior.css" media="all" rel="stylesheet" type="text/css" />
 	
-	<script src="js/vendor/modernizr.js"></script>
-	
-
+	<script src="js/vendor/modernizr.js"></script>	
 </head>
 
 <body>
