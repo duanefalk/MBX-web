8ae423344cd3cbeba8d69a05e03e378226627d26
duanefalk <duanejfalk@yahoo.com>
2014-07-25 16:00:59 -0400

    Fixed text format on manage collections

--- Collection_Value_Lists.php
+++ Collection_Value_Lists.php
@@ -13,23 +13,18 @@
             </td>
             <td id="page">
                 <h2>Manage Your Code Lists</h2>
-                <p>Collection code lists can speed up adding models to your collection by letting you select from a dropdown list of sellers and storage locations, </>
-                <p>rather than having to type those in with each model. In the add/update/delete functions you will first be asked whether you want to work with</p>
-                <p>'Sellers' or 'Locations'</p><br />
+                <p>Collection code lists can speed up adding models to your collection by letting you select from a dropdown list of sellers and storage locations, rather than having to type those in with each model. In the add/update/delete functions you will first be asked whether you want to work with 'Sellers' or 'Locations'</p>
 
-                <p>Sellers are, of course the people or companies from whom you buy your models. Typically there will be a core group of sellers you use repeatedly,</p>
-                <p>so selecting them from a list will be quicker than typing out their names.</>
-                <p>There are two fields to enter for sellers:</p>
-                <p>a) Seller Name:   The name of the person or company</p>
-                <p>b) Display Order: It is more convenient to have the most commonly used sellers at the front of the list. You can enter a number here to set the order</>
-                <p>                  they display in the list. I would recommend leaving gaps in the numbers between eaxh seller for future additions.</p><br />
-
-                <p>'Locations' refers to storage locations for your models. In the collection table you are given two fields for these, which allows you to enter a main</p>
-                <p>location and a sublocation (e.g. storage unit1, shelf 3).</p>
-                <p>As with sellers, there are two fields to enter:</p>
-                <p>a) Location Name: What you call your storage locations. You need to include both main locations and sublocations in the list, if you have them.</p>
-                <p>b) Display Order: As above, the order the locations display in the drop-down. You may want to group your main locations first, then you sub-locations.</p>
-
+                <p>Sellers are, of course the people or companies from whom you buy your models. Typically there will be a core group of sellers you use repeatedly, so selecting them from a list will be quicker than typing out their names. There are two fields to enter for sellers:</p>
+                <ul>
+                    <li>Seller Name:   The name of the person or company</li>
+                    <li>Display Order: It is more convenient to have the most commonly used sellers at the front of the list. You can enter a number here to set the order they display in the list. I would recommend leaving gaps in the numbers between eaxh seller for future additions.</li>
+                </ul>
+                <p>'Locations' refers to storage locations for your models. In the collection table you are given two fields for these, which allows you to enter a main location and a sublocation (e.g. storage unit1, shelf 3). As with sellers, there are two fields to enter:</p>
+                <ul>
+                    <li>Location Name: What you call your storage locations. You need to include both main locations and sublocations in the list, if you have them.</li>
+                    <li>Display Order: As above, the order the locations display in the drop-down. You may want to group your main locations first, then you sub-locations.</li>
+                </ul>
             </td>
     </tr>
 </table>
