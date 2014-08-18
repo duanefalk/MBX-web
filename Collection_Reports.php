16fcd10de94b89244bfd548879b49bd75dbd8e27
duanefalk <duanejfalk@yahoo.com>
2014-08-18 12:41:10 -0400

    Updated Collection_reports
    Made format consistent, added a set of reports to work on

--- Collection_Reports.php
+++ Collection_Reports.php
@@ -2,21 +2,31 @@
 
 <div class="row">
 	<div class="large-12 columns">
-		<a href="Collection Report_A" class="button dark">Collection Summary</a>
-		<a href="Collection_Report_B.php" class="button dark">Collection by Year</a>
-        <a href="Collection_Report_C.php" class="button dark">etc etc</a>
 		<a href="Collections_Menu.php" class="button dark">Return to Collections Menu</a>
-		<a href="index.php" class="button dark">Return to Main Page</a>	
+		<a href="index.php" class="button dark">Return to Main Page</a>
+		
+		<h2>Collection Reports</h2>
+		<p>Select a report from the menu, fill in the specific data as required, and view or print.</p>
 	</div>
 </div>
 
 <div class="row">
-	<div class="large-12 columns">
-		
-		<h2>Collection Reports</h2>
-		<p>Select a report from the menu, fill int he specific data and view, print or download.</p>
-		
-	</div>
+	<div class="large-3 columns sidebar">
+		<a href="Collection Report_A">Collection Summary</a>
+		<a href="Collection Report_B">Models from a Seller</a>
+		<a href="Collection Report_C">Summary for all Sellers</a>
+		<a href="Collection Report_D">List all variations of a Model- by UMID</a>
+		<a href="Collection Report_E">List all variations of a Model- by MAN#</a>
+		<a href="Collection Report_F">List all variations of a Model- by Mack #- regular wheels (i.e. 5-a)</a>
+		<a href="Collection Report_G">List all variations of a Model- by Mack #- superfast (i.e. 38-e)</a>
+		<a href="Collection Report_H">Summary for all Sellers</a>
+		<a href="Collection Report_I">Models in poor/fair condition</a>
+		<a href="Collection Report_J">Models by release Series</a>
+		<a href="Collection_Report_K.php">Collection by Year</a>
+	</div>	
+
 </div>
+
+
 		
 <?php include("includes/footer.php"); ?>
