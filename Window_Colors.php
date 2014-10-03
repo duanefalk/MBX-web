9ac74c6933f8af854200d44a64e254d3430b0618
duanefalk <duanejfalk@yahoo.com>
2014-10-03 15:44:31 -0400

    Added window_color.php
    Added code and also a set of window color photos under ref_images

--- Window_Colors.php
+++ Window_Colors.php
@@ -1,20 +1,40 @@
+<?php ob_start(); ?>
+<?php require_once("includes/db_connection.php"); ?>
 <?php include("includes/header.php"); ?>
+<?php include("includes/functions.php"); ?>
 
 <div class="row">
 	<div class="large-12 columns">
-	<a href="Reference_List.php" class="button dark">Photos Sources & Other Links</a>
-        <a href="Window_Colors.php" class="button dark">Window Colors</a>
-        <a href="Wheel_Types.php" class="button dark">Wheel Types</a>	
-	<a href="Theme_List.php" class="button dark">Release Themes</a>
-	<a href="Series_List.php" class="button dark">Release Series List</a>
-        <a href="Collections_Menu.php" class="button dark">Return to Collections Menu</a>	
+		<h2>Window Colors</h2>
+		<p>Photo and of some of the more common window colors used for Matchbox vehicles. Window colors are listed in the bdatabase with 2 characteristics: color and transparency, i.e. 'colorless/clear' is the typical clear window that you can see through.</p>
 	</div>
 </div>
-
 <div class="row">
 	<div class="large-12 columns">
-		<h2>Reference Files</h2>
-		<p>Information about and photos of various components that are used in the database.</p>
+		<h4>Transparency</h4>
+		<p></p>
+		<p>clear: <img src="images/ref_images/clear.jpg" align="middle" width=120></p>
+		<p>clouded: <img src="images/ref_images/clouded.jpg" align="middle" width=120></p>
+		<p>opaque: <img src="images/ref_images/opaque.jpg" align="middle" width=120></p>
+		
+		
+		<h4>Common Colors</h4>
+		<p>Most of these can appear in clear, clouded or opaque variations. White can be cloudy or opaque and the last two (black, chrome) are always opaque. Some color variations are very similar, such as pink & purple, green-blue & green. Hold the model up so light is shining through the windows to help distringuish the color.</p>
+		<p>colorless: <img src="images/ref_images/colorless.jpg" align="middle" width=120></p>
+		<p>smoke:<img src="images/ref_images/smoke.jpg" align="middle" width=120></p>
+		<p>amber:<img src="images/ref_images/amber.jpg" align="middle" width=120></p>
+		<p>yellow:<img src="images/ref_images/yellow.jpg" align="middle" width=120></p>
+		<p>orange:<img src="images/ref_images/orange.jpg" align="middle" width=120></p>
+		<p>blue:<img src="images/ref_images/blue.jpg" align="middle" width=120></p>
+		<p>blue-green:<img src="images/ref_images/blue-green.jpg" align="middle" width=120></p>
+		<p>green:<img src="images/ref_images/green.jpg" align="middle" width=120></p>
+		<p>yellow-green:<img src="images/ref_images/yellow-green.jpg" align="middle" width=120></p>
+		<p>red:<img src="images/ref_images/red.jpg" align="middle" width=120></p>
+		<p>pink:<<img src="images/ref_images/pink.jpg" align="middle" width=120></p>
+		<p>purple:<img src="images/ref_images/purple.jpg" align="middle" width=120></p>
+		<p>white:<img src="images/ref_images/white.jpg" align="middle" width=120></p>
+		<p>black:<img src="images/ref_images/black.jpg" align="middle" width=120></p>
+		<p>chrome:<img src="images/ref_images/chrome.jpg" align="middle" width=120></p>
 	</div>
 </div>
 
