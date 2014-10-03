b9a0b9bb18b5cfdcb3395b574ec68cc081d166a0
duncanfalk <djf@duncanfalk.com>
2014-10-03 11:58:14 -0400

    Collections_Menu; Manage Models;
    Collections Menu formatting;
    Manage_Models_in_Collection: new layout;
    Search_Models_Menu: removing 2nd instance of utility-nav;

--- Collections_Menu.php
+++ Collections_Menu.php
@@ -4,107 +4,90 @@
 ?>
 
 <?php require_once("includes/db_connection.php"); ?>
-<?php include("includes/navUtility.php"); ?>
 <?php include("includes/header.php"); ?>
 <?php require_once("includes/functions.php"); ?>
 
 <?php 	$Username= $_SESSION['Username'];
 	$SecLvl=$_SESSION['Sec_Lvl'];
-	$check= WEB_IMAGE_URL . "iconCheck.png";
-	$X_out= WEB_IMAGE_URL . "iconCross.png";
+	$check = WEB_IMAGE_URL . "iconCheck.png";
+	$X_out = WEB_IMAGE_URL . "iconCross.png";
 ?>
 
 <div class="row">
-
 	<div class="large-12 columns">
 	
 		<h2>Your Collections</h2>
 		<p>Steps to set up a collection using this web site and database:</p>
 		
 		<h3>Step 1: Set up an Account</h3>	
-			<?php
+		<?php
+
+			if ($SecLvl > 1) {
+				echo "<p><img class='icon inline' src=".$check." width=\"40\"> You are all set with an account, proceed to step 2.</p>";
+			} else {
+				echo "<p><img class='icon inline' src=".$X_out." width=\"40\"> Go to the Account option from the Main Menu and set up and account, then return here.</p>";					
+			}
+		?>
 	
-				if ($SecLvl > 1) {
-					echo "<img src=".$check." width=\"40\">";
-					echo "You are all set with an account, proceed to step 2";
-				} else {
-					echo "<img src=".$X_out." width=\"40\">";
-					echo "Go to the Account option from the Main Menu and set up and account, then return here.";					
-				}
-			?>
-		
 		<h3>Step 2: Create a Collection under your account</h3>
-			<?php
-				$result = 0;
-				$rows_count = 0;
-				$query = ("SELECT * FROM Matchbox_User_Collections WHERE Username='$Username'");													
-				$result = mysql_query($query);
-				$rows_count = mysql_num_rows($result);
-		                if ($rows_count == 0) {
-					echo "<img src=".$X_out." width=\"40\">";
-					echo "Go to 'Create a Collection' (from 'Manage Collections', on this page) and set up a new collection.";
-				}
-				else {				
-					echo "<img src=".$check." width=\"40\">";
-					echo "You've already created a collection, proceed to step 3";		
-				}
-			?>
-	
-			<h3>Step 3 (optional): Set up lists of sellers and storage locations</h3>
-				<?php
-					//fake until set up
-					$result=0;
-					$rows_count=0;
-					$query=("SELECT * FROM Matchbox_User_Coll_Value_Lists WHERE Username= '$Username'");													
-					$result = mysql_query($query);
-					$rows_count= mysql_num_rows($result);
-			                if ($rows_count == 0) {
-						echo "<img src=".$X_out." width=\"40\">";
-						echo "No sellers or storage locations added yet."."<br></>";
-						echo "If you want to have your own sellers and storage locations for your collection (and don't want to type them in each time),"."<br></>";
-						echo "Go to 'Set Up Collection Value Lists' (from 'Manage Collections', on this page) and enter the values you desire. These will show up in drop-down"."<br></>";
-						echo "lists that you can select from, for the appropriate fields when you are adding models rather than you needing to type them in."."<br></><br></>";	
-					}
-					else {
-						echo "<img src=".$check." width=\"40\">";
-						echo "You've already entered some collection value list items. You can add to your lists at any time form the 'Create Collection Value"."<br></>";
-						echo "Lists' menu option."."<br></><br></>";
-					}
-				?>
-	
-			<h3>Step 4: Add models to your collection</h3>
-				<?php
-					$result_mdls=0;
-					$rows_count=0;
-					$query_mdls= ("SELECT * FROM Matchbox_Collection WHERE Username= '$Username'");
-					$result_mdls= mysql_query($query_mdls);
-					$rows_count= mysql_num_rows($result);
-			                if ($rows_count == 0) {
-					//$Collection_not_null=0;
-					//if ($Collection_not_null) {
-						//echo "<img src=/" . $check . " width=\"40\">";
-						echo "<img src=/" . $check . " width=\"40\">";
-						echo "You've already started adding models. Enjoy growing your collection!";
-					} else {
-						echo "<img src=" . $X_out . " width=\"40\">";
-						echo "No models yet added"."<br></>";
-						echo "There are two ways to add a model to your collection:"."<br></><br></>";
-						echo "Option 1: Do a model/release search, then add from the search result."."<br></>";
-						echo "First look up the model you have from the 'Search Models' or 'Search Releases' menu options ('Main' page)."."<br></>";
-						echo "Then drill down to the variation record that matches your model. On the 'Variations Detail' page, check the box marked 'Add to"."<br></>";
-						echo "Collection' next to the photo of the correct variation. Fill in the collection information in the resulting form, and submit it."."<br></>";
-						echo "The model is now added to your collection."."<br></><br></>";
-						echo "Note: you can search by any criteria on the search pages to get to the variation you want."."<br></>";
-						echo "<br></br>";
-						echo "Option 2:  Direct entry"."<br></>";
-						echo "If you know the database 'Variation ID' (also called 'VarID') of the model you have (for instance, 'SF0484-003-a') you can "."<br></>";
-						echo "enter it directly from the 'Manage Your Collection' menu. Using this option, you MUST have this ID- you cannot use "."<br></>";
-						echo "just the MAN#, Mack# or other ID for this method. You can print listings of models with the VarID's to use as reference if you"."<br></>";
-						echo " prefer this method.";
-						
-					}
-				?>
-	
+		<?php
+			$result = 0;
+			$rows_count = 0;
+			$query = ("SELECT * FROM Matchbox_User_Collections WHERE Username='$Username'");													
+			$result = mysql_query($query);
+			$rows_count = mysql_num_rows($result);
+	                if ($rows_count == 0) {
+				echo "<p><img class='icon inline' src=".$X_out." width=\"40\"> Go to 'Create a Collection' (from 'Manage Collections', on this page) and set up a new collection.</p>";
+			}
+			else {				
+				echo "<p><img class='icon inline' src=".$check." width=\"40\"> You've already created a collection, proceed to step 3</p>";		
+			}
+		?>
+
+		<h3>Step 3 (optional): Set up lists of sellers and storage locations</h3>
+		<?php
+			//fake until set up
+			$result=0;
+			$rows_count=0;
+			$query=("SELECT * FROM Matchbox_User_Coll_Value_Lists WHERE Username= '$Username'");													
+			$result = mysql_query($query);
+			$rows_count= mysql_num_rows($result);
+	        
+	        if ($rows_count == 0) {
+				echo "<p><img class='icon inline' src=".$X_out." width=\"40\"> No sellers or storage locations added yet."."</p>";
+				echo "<p>If you want to have your own sellers and storage locations for your collection (and don't want to type them in each time), Go to 'Set Up Collection Value Lists' (from 'Manage Collections', on this page) and enter the values you desire. These will show up in drop-down lists that you can select from, for the appropriate fields when you are adding models rather than you needing to type them in.</p>";	
+			}
+			else {
+				echo "<p><img class='icon inline' src=".$check." width=\"40\"> You've already entered some collection value list items. You can add to your lists at any time from the 'Create Collection Value Lists menu option.</p>";
+			}
+		?>
+
+		<h3>Step 4: Add models to your collection</h3>
+		<?php
+			$result_mdls=0;
+			$rows_count=0;
+			$query_mdls= ("SELECT * FROM Matchbox_Collection WHERE Username= '$Username'");
+			$result_mdls= mysql_query($query_mdls);
+			$rows_count= mysql_num_rows($result);
+	                if ($rows_count == 0) {
+			//$Collection_not_null=0;
+			//if ($Collection_not_null) {
+				//echo "<img src=/" . $check . " width=\"40\">";
+				echo "<p><img class='icon inline' src=" . $check . " width=\"40\"> You've already started adding models. Enjoy growing your collection!</p>";
+			} else {
+				echo "<p><img class='icon inline' src=" . $X_out . " width=\"40\"> No models yet added</p>";
+				echo "<p>There are two ways to add a model to your collection:</p>";
+				echo "<p><strong>Option 1:</strong> Do a model/release search, then add from the search result.</p>";
+				echo "<p>First look up the model you have from the 'Search Models' or 'Search Releases' menu options ('Main' page).</p>";
+				echo "<p>Then drill down to the variation record that matches your model. On the 'Variations Detail' page, check the box marked 'Add to Collection' next to the photo of the correct variation. Fill in the collection information in the resulting form, and submit it.</p>";
+				echo "<p>The model is now added to your collection.</p>";
+				echo "<p>Note: you can search by any criteria on the search pages to get to the variation you want.</p>";
+				echo "<br></br>";
+				echo "<p><strong>Option 2:</strong> Direct entry.</p>";
+				echo "<p>If you know the database 'Variation ID' (also called 'VarID') of the model you have (for instance, 'SF0484-003-a') you can enter it directly from the 'Manage Your Collection' menu. Using this option, you MUST have this ID- you cannot use just the MAN#, Mack# or other ID for this method. You can print listings of models with the VarID's to use as reference if you prefer this method.</p>";						
+			}
+		?>	
+		
 	</div>
 </div>
 
--- Manage_Models_in_Collection.php
+++ Manage_Models_in_Collection.php
@@ -4,16 +4,25 @@
 
 <div class="row">
 	<div class="large-12 columns">
-		<a href="Dir_Add_Mdl_to_Coll.php" class="button dark">Add a Model</a>
-        <a href="Updt_Coll_Mdl.php" class="button dark">View/Update a Model</a>
-        <a href="Del_Mdls_in_Coll.php" class="button dark">Delete a Model</a>
-        <a href="Collections_Menu.php" class="button dark">Return to Collections Menu</a>	
+		<h2>Manage the Models in Your Collection</h2>
 	</div>
+</div>
+
+<div class="row actionButtons">
+	<div class="large-4 columns">
+		<a href="Dir_Add_Mdl_to_Coll.php" class="button dark">Add a Model</a>
+	</div>
+	<div class="large-4 columns">
+        <a href="Updt_Coll_Mdl.php" class="button dark">View/Update a Model</a>
+    </div>
+    <div class="large-4 columns">   
+        <a href="Del_Mdls_in_Coll.php" class="button dark">Delete a Model</a>
+    </div>
 </div>
 
 <div class="row">
 	<div class="large-12 columns">
-		<h2>Manage the Models in Your Collection</h2>
+        <a href="Collections_Menu.php" class="button dark block">Return to Collections Menu</a>	
 	</div>
 </div>
 
