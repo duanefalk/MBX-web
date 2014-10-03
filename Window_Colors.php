8706d59ac684bea471275ccdddd2bb414993374f
duncanfalk <djf@duncanfalk.com>
2014-10-03 16:33:07 -0400

    Window Colors formatting

--- Window_Colors.php
+++ Window_Colors.php
@@ -12,30 +12,104 @@
 <div class="row">
 	<div class="large-12 columns">
 		<h4>Transparency</h4>
-		<p></p>
-		<p>clear: <img src="images/ref_images/clear.jpg" align="middle" width=120></p>
-		<p>clouded: <img src="images/ref_images/clouded.jpg" align="middle" width=120></p>
-		<p>opaque: <img src="images/ref_images/opaque.jpg" align="middle" width=120></p>
-		
-		
-		<h4>Common Colors</h4>
-		<p>Most of these can appear in clear, clouded or opaque variations. White can be cloudy or opaque and the last two (black, chrome) are always opaque. Some color variations are very similar, such as pink & purple, green-blue & green. Hold the model up so light is shining through the windows to help distringuish the color.</p>
-		<p>colorless: <img src="images/ref_images/colorless.jpg" align="middle" width=120></p>
-		<p>smoke:<img src="images/ref_images/smoke.jpg" align="middle" width=120></p>
-		<p>amber:<img src="images/ref_images/amber.jpg" align="middle" width=120></p>
-		<p>yellow:<img src="images/ref_images/yellow.jpg" align="middle" width=120></p>
-		<p>orange:<img src="images/ref_images/orange.jpg" align="middle" width=120></p>
-		<p>blue:<img src="images/ref_images/blue.jpg" align="middle" width=120></p>
-		<p>blue-green:<img src="images/ref_images/blue-green.jpg" align="middle" width=120></p>
-		<p>green:<img src="images/ref_images/green.jpg" align="middle" width=120></p>
-		<p>yellow-green:<img src="images/ref_images/yellow-green.jpg" align="middle" width=120></p>
-		<p>red:<img src="images/ref_images/red.jpg" align="middle" width=120></p>
-		<p>pink:<<img src="images/ref_images/pink.jpg" align="middle" width=120></p>
-		<p>purple:<img src="images/ref_images/purple.jpg" align="middle" width=120></p>
-		<p>white:<img src="images/ref_images/white.jpg" align="middle" width=120></p>
-		<p>black:<img src="images/ref_images/black.jpg" align="middle" width=120></p>
-		<p>chrome:<img src="images/ref_images/chrome.jpg" align="middle" width=120></p>
 	</div>
+</div>
+
+<div class="row">
+	<div class="large-3 columns text-center">
+		<h5>Clear</h5>
+		<p><img src="images/ref_images/clear.jpg" width="120"></p>
+	</div>
+	
+	<div class="large-3 columns text-center">
+		<h5>Clouded</h5>
+		<p><img src="images/ref_images/clouded.jpg" width="120"></p>
+	</div>
+	
+	<div class="large-3 columns start text-center">
+		<h5>Opaque</h5>
+		<p><img src="images/ref_images/opaque.jpg" width="120"></p>
+	</div>	
+</div>
+
+
+<div class="row">
+	<div class="large-12 columns">		
+		<h4>Common Colors</h4>
+		<p>Most of these can appear in clear, clouded or opaque variations. White can be cloudy or opaque and the last two (black, chrome) are always opaque. Some color variations are very similar, such as pink & purple, green-blue & green. Hold the model up so light is shining through the windows to help distinguish the color.</p>
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-3 columns text-center">
+		<h5>Colorless</h5>
+		<p><img src="images/ref_images/colorless.jpg" width="120"></p>
+	</div>	
+	<div class="large-3 columns text-center">
+		<h5>Smoke</h5>
+		<p><img src="images/ref_images/smoke.jpg" width="120"></p>
+	</div>	
+	<div class="large-3 columns text-center">
+		<h5>Amber</h5>
+		<p><img src="images/ref_images/amber.jpg" width="120"></p>
+	</div>
+	<div class="large-3 columns text-center">
+		<h5>Yellow</h5>
+		<p><img src="images/ref_images/yellow.jpg" width="120"></p>
+	</div>	
+</div>
+
+<div class="row">
+	<div class="large-3 columns text-center">
+		<h5>Orange</h5>
+		<p><img src="images/ref_images/orange.jpg" width="120"></p>
+	</div>	
+	<div class="large-3 columns text-center">
+		<h5>Blue</h5>
+		<p><img src="images/ref_images/blue.jpg" width="120"></p>
+	</div>
+	<div class="large-3 columns text-center">
+		<h5>Blue-Green</h5>
+		<p><img src="images/ref_images/blue-green.jpg" width="120"></p>
+	</div>	
+	<div class="large-3 columns text-center">
+		<h5>Green</h5>
+		<p><img src="images/ref_images/green.jpg" width="120"></p>
+	</div>	
+</div>
+
+<div class="row">
+	<div class="large-3 columns text-center">
+		<h5>Yellow-Green</h5>
+		<p><img src="images/ref_images/yellow-green.jpg" width="120"></p>
+	</div>
+	<div class="large-3 columns text-center">
+		<h5>Red</h5>
+		<p><img src="images/ref_images/red.jpg" width="120"></p>
+	</div>	
+	<div class="large-3 columns text-center">
+		<h5>Pink</h5>
+		<p><img src="images/ref_images/pink.jpg" width="120"></p>
+	</div>
+	<div class="large-3 columns text-center">
+		<h5>Purple</h5>
+		<p><img src="images/ref_images/purple.jpg" width="120"></p>
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-3 columns text-center">
+		<h5>White</h5>
+		<p><img src="images/ref_images/white.jpg" width="120"></p>
+	</div>	
+	<div class="large-3 columns text-center">
+		<h5>Black</h5>
+		<p><img src="images/ref_images/black.jpg" width="120"></p>
+	</div>	
+	<div class="large-3 columns start text-center">
+		<h5>Chrome</h5>
+		<p><img src="images/ref_images/chrome.jpg" width="120"></p>
+	</div>	
 </div>
 
 <!-- Sub Menu -->
