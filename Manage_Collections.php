212033ea7a8016e0af56bcfd6b4eb41df8f5760e
duncanfalk <djf@duncanfalk.com>
2014-11-11 16:44:10 -0500

    Formadding Collections pages

--- Manage_Collections.php
+++ Manage_Collections.php
@@ -1,21 +1,29 @@
 <?php require_once("includes/db_connection.php"); ?>
 <?php include("includes/header.php"); ?>
 <?php require_once("includes/functions.php"); ?>
-
-<div class="row">
-	<div class="large-12 columns">
-		<a href="Create_Collection.php" class="button dark">Create a Collection</a>
-		<a href="Collection_Value_Lists.php" class="button dark">Set up Collection Code Lists</a>
-		<a href="Update_Collection.php" class="button dark">View/Update Your Collections</a>
-		<a href="Delete_Collection.php" class="button dark">Delete a Collection</a>
-		<a href="Collections_Menu.php" class="button dark">Return to Collections Menu</a>
-	</div>
-</div>
             
-<div class="row">
-	<div class="large-12 columns">
-		<h2>Create/Configure Your Collections</h2>
+<section class="row">
+	<h2>Create/Configure Your Collections</h2>
+	
+	<div class="row actionButtons">
+		<div class="large-4 columns">
+			<a href="Create_Collection.php" class="button dark">Create a Collection</a>
+		</div>
+		<div class="large-4 columns">
+	    	<a href="Collection_Value_Lists.php" class="button dark">Set up Collection Code Lists</a>
+	    </div>
+	    <div class="large-4 columns">   
+	        <a href="Update_Collection.php" class="button dark">View/Update Your Collections</a>
+	    </div>
 	</div>
-</div>
+	<div class="row actionButtons">
+		<div class="large-4 columns">
+			<a href="Delete_Collection.php" class="button dark">Delete a Collection</a>
+		</div>
+		<div class="large-4 columns end">
+	    	<a href="Collections_Menu.php" class="button dark">Return to Collections Menu</a>
+	    </div>
+	</div>
+</section>
 
 <?php include("includes/footer.php"); ?>
--- Manage_Models_in_Collection.php
+++ Manage_Models_in_Collection.php
@@ -12,14 +12,14 @@
 	<div class="large-4 columns">
 		<a href="Dir_Add_Mdl_to_Coll.php" class="button dark">Add a Model</a>
 	</div>
-	<div class="large-4 columns">
+	<div class="large-4 columns end">
         <a href="Updt_Coll_Mdl.php" class="button dark">View/Update/Delete a Model</a>
     </div>
 
 </div>
 
 <div class="row">
-	<div class="large-12 columns">
+	<div class="large-4 large-centered columns end">
         <a href="Collections_Menu.php" class="button dark block">Return to Collections Menu</a>	
 	</div>
 </div>
