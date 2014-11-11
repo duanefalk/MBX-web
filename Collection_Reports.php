779ddbbdc408065c51e83bd5a383192b7bd446a6
duncanfalk <djf@duncanfalk.com>
2014-11-11 16:48:32 -0500

    Reports formatting

--- Collection_Reports.php
+++ Collection_Reports.php
@@ -1,32 +1,54 @@
 <?php include("includes/header.php"); ?>
 
-<div class="row">
+<section class="row">
 	<div class="large-12 columns">
 		<a href="Collections_Menu.php" class="button dark">Return to Collections Menu</a>
 		<a href="index.php" class="button dark">Return to Main Page</a>
 		
 		<h2>Collection Reports</h2>
 		<p>Select a report from the menu, fill in the specific data as required, and view or print.</p>
+		
+		<div class="row actionButtons">
+			<div class="large-4 columns">
+				<a href="Collection_Report_A" class="button dark">Collection Summary</a>
+			</div>
+			<div class="large-4 columns">
+		    	<a href="Collection Report_B" class="button dark">Models from a Seller</a>
+		    </div>
+		    <div class="large-4 columns">   
+		        <a href="Collection Report_C" class="button dark">Summary for all Sellers</a>
+		    </div>
+		</div>
+		<div class="row actionButtons">
+			<div class="large-4 columns">   
+		        <a href="Collection Report_D" class="button dark">List all variations of a Model- by UMID</a>
+		    </div>
+		    <div class="large-4 columns">   
+		        <a href="Collection Report_E" class="button dark">List all variations of a Model- by MAN#</a>
+		    </div>
+		    <div class="large-4 columns">   
+		        <a href="Collection Report_F" class="button dark">List all variations of a Model by Mack # <small>ex.: regular wheels (i.e. 5-a)</small></a>
+		    </div>
+		</div>
+		<div class="row actionButtons">
+			<div class="large-4 columns">   
+		        <a href="Collection_Report_H" class="button dark">Summary for all Sellers</a>
+		    </div>
+		    <div class="large-4 columns">   
+		        <a href="Collection Report_I" class="button dark">Models in poor/fair condition</a>
+		    </div>
+		    <div class="large-4 columns">   
+		        <a href="Collection Report_J" class="button dark">Models by release Series</a>
+		    </div>
+		</div>
+		<div class="row actionButtons">
+			<div class="large-4 columns end">   
+		        <a href="Collection_Report_K.php" class="button dark">Collection by Year</a>
+		    </div>
+		</div>
+		
+		
 	</div>
-</div>
-
-<div class="row">
-	<div class="large-3 columns sidebar">
-		<a href="Collection_Report_A">Collection Summary</a>
-		<a href="Collection Report_B">Models from a Seller</a>
-		<a href="Collection Report_C">Summary for all Sellers</a>
-		<a href="Collection Report_D">List all variations of a Model- by UMID</a>
-		<a href="Collection Report_E">List all variations of a Model- by MAN#</a>
-		<a href="Collection Report_F">List all variations of a Model- by Mack #- regular wheels (i.e. 5-a)</a>
-		<a href="Collection Report_G">List all variations of a Model- by Mack #- superfast (i.e. 38-e)</a>
-		<a href="Collection_Report_H">Summary for all Sellers</a>
-		<a href="Collection Report_I">Models in poor/fair condition</a>
-		<a href="Collection Report_J">Models by release Series</a>
-		<a href="Collection_Report_K.php">Collection by Year</a>
-	</div>	
-
-</div>
-
-
+</section>
 		
 <?php include("includes/footer.php"); ?>
