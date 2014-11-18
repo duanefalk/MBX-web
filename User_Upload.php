4ec6d390afc7f17ccf4e3d96b3bd868fbe00b51d
duncanfalk <djf@duncanfalk.com>
2014-11-18 17:53:49 -0500

    Upload File formatting

--- User_Upload.php
+++ User_Upload.php
@@ -1,28 +1,25 @@
 <?php require_once("includes/db_connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
 <?php include("includes/header.php"); ?>
-<table id="structure">
-<tr>
-	<td id="navigation">
-	
-	<a href="Main_Page"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Main Page</p</a>
-	</td>
-	<td id="page">
+
+<div class="row">
+	<div class="large-12">
 		<h2>Upload a File</h2>		
 
 		<form action="User_Upload_Record.php" method="post" enctype="multipart/form-data">
-			<p>Your Name:     	  <input type="text" name="user_name" value="" size="50" id="user_name"</p>
-			<p>Comment:     </p>
-				<textarea name="user_comment" cols="45" rows="4">	
-				</textarea>
-			<br />	
+			<label for="user_name">Your Name:</label>
+			<input type="text" name="user_name" value="" size="50" id="user_name">
+			
+			<label for="comment">Comment:</label>
+			<textarea id="comment" name="user_comment"></textarea>
+				
 			<label for="file">Filename:</label>
-			<input type="file" name="upload" id="file" <br />
-			<input type="submit" name="submit" id="submit" value="Upload">
-			<br />	
+			<input type="file" name="upload" id="file">
+			
+			<input type="submit" name="submit" id="submit" value="Upload" class="button dark">
+			<a class="button cancel" href="User_Upload.php">Cancel</a>
 		</form>
-		<a href="User_Upload.php">Cancel</a>
-	</td>
-</tr>
-</table>
+	</div>
+</div>
+
 <?php require("includes/footer.php"); ?>
