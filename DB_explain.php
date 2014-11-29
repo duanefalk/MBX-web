68eecfb35094750a927fdef9384f810dd8a41fcc
duanefalk <duanejfalk@yahoo.com>
2014-11-28 19:52:58 -0500

    Expanded the 'about the database' page text

--- DB_explain.php
+++ DB_explain.php
@@ -63,11 +63,65 @@
 			<li>Other ID numbers: Mack no., MAN/FAB#</li>
 			<li>Version name: Since there may be multiple traditional models under one UMID, different versions may have different version names (i.e. Under the 'Big Banger' model, there is that version, the Cosmic Blues version and others.</li>
 			<li>Year the Version was First released</li>
-			<li>The color(s), tampo deisng and tampo text on the version</li>
+			<li>The color(s), tampo deisgn and tampo text on the version</li>
 			<li>Version attachments, when appropriate</li>
 			<li>Code Level and Secondary Manufacturer when appropriate: Code 1, made by Mattel (or LEsney etc), Code 2 design applied by an approved secondary company (i.e. CCI). I have not included code 3 (custom models made without approval of Mattel)</li>
+		
+		<h4>VARIATION</h4>
+		<h6>What is a Variation?</h6>
+		<p>Within a version there may be smaller differences that form variations of that version. Details such as:</p>
+		<ol>
+			<li>Wheel style: often Matchbox used different wheels for the same model, perhaps using up what was lying around the shop. Each different wheel style or combination is a variation. Note- both front wheel and rear wheels are considered. CHaracteristics include design of the hub, color of tire and hub, rubber or plastic tires.</li>
+			<li>Windows: There are often different colors of windows used for the same model. There is a separate page- <a href="Window_Colors.php" class="button dark">WINDOW COLORS</a> -that shows the most common vaiations</li>
+			<li>Base Details: Plastic or Metal, color, details on the base etc. A model can have more than one base- an inset, a trailer base etc. and both are described.</li>
+			<li>Color or Tampo variations: small variations in color may occur for the same version. SImilarly, some models aare obviously of the same tampo design, but have a differece- ie. inclusion or lack of the MB2000, 50 or Hero City logos on models form 2000-2004.</li>
+			<li>Accessory differences: Whether or not the model has a tow hook, color of a driver etc.</li>
+		</ol>
+		<p>Each variation in the database has these characteristics:</p>
+		<ul>
+			<li>Unique Variation ID (VARID) made up of the UMID + VerID + a letter for the variation, i.e. SF550-001-a and SF550-001-b are 2 variations of the same model.</li>
+			<li>Specific Mack# form C. Mack;s latest catalog versions</li>
+			<li>Model name on the base- this often differs among versions and even sometimes among variations</li>
+			<li>Manufactourer location: Where the model was made- England, China etc.</li>
+			<li>Wheel styles- front and back</li>
+			<li>Base Type: plastic or metal</li>
+			<li>Base color</li>
+			<li>Window color</li>
+			<li>Interior color</li>
+			<li>Color or tampo variations</li>
+			<li>Miscellaneous details: knowing that I couldn;t predict every possible way 2 cars could differ, I left 5 generic fields for additional details.Includes the detail type (i.e. what aspect of the vehicle is different) and the detail variation (the specific of that detail); for example- 
+			<p>DetTyp1: 	kayak color    	DetVar1: Yellow</p>
+			<p>Note: with some models, especially older ones, there are small structural details that collectores have identified. Some of these require the model to be dismantled to see! If I couldn't easily tell the differences between two identified variations, I listed these separately as 'microvariations'. a link on the search result spage show you all microvariations identified for a model.</p>
 		</ul>
 
+		<h4>RELEASE</h4>
+		<h6>What is a Release?</h6>
+		<p>A release is the way that a given model was sold- its series, packaging etc. Many collectors eschew this level of detail (they don;t collect 'cardboard', as the saying goes), but it is often useful for identifying a model or to help you search for one on ebay, to know how the model was packaged. Each variation then has one or more releases. The release includes both the series that a model as sold under (i.e. 1-75, Playset, Promotion), the market it was sold in (USA, Rrest of world or 'ROW' or perhaps specific countries) and the year is was offered. So a model offered as #50 in the 1-75 series in the USA in 1984 and also in EUrope, it would have 2 releases.</p>
+		<p>Each release in the database has these characteristics:</p>
+		<ul>
+			<li>Unique release ID (RelID) made up of the variation ID + a 2 difit number for the release.</li>
+			<li>Release Year</li>
+			<li>Country of sale: If sold in a particular country it can be listed as such (i.e. promos only sold in Belgium). USA is the biggest example of this. Aggregations of markets include ROW- rest of world (anywhere but USA), LA- Latin America, and if generally available, CORE- offered in all major markets.</li>
+			<li>Line: Miniatures, SKybusters, King/Superking, COnoys etc. The highest level distinction of releases. This site focuses on miniatures, but many miniature models are offered under SKybusters etc, and those are included.</li>
+			<li>Theme: Matchbox has sometimes used a particular theme for its packaging and sales across different series and years. Examples include the Tyco 'Fast Lane' theme, the much-loved 'Hero City' theme, and starting in 2014, 'On a Mission'. A list of themes can be reviewed at <a href="Theme_List.php" class="button dark">RELEASE THEMES</a></li>	
+			<li>Series: 1-75, Multi-packs, Gift Sets, Premiere models etc. A list can be found in <a href="Series_List.php" class="button dark">RELEASE SERIES LIST</a></li>
+			<li>Series ID: if a series is numbered, such as the 1-75 models, that number.</li>
+			<li>Subseries and subseries ID: In recent years Mattel has dividede the 1-7 set into various subsets, Emergency, Construction etc. Other example sinclude the various 'World Class Series' under Premiere models</li>
+			<li>Package ID & Code: Mattel, and to some extent Tyco, include package ID#'s on their packages. These are typically 5 digit numbers or letter-number combos. The pkg ID often differs for the same model in the USA and other countries. The package code is a general style of the package. For 1-75 these are based on the common Box or BLister types (i.e. Box F)</li>
+			<li>Package name: What the package is called, if it is unqieuly identified. Not to be confused with the series or subseries name listed on the package, For instance, 'Exotic Rides' on a 5-pack is a package name, whereas Premiere and World Class Series 5 are series and subseries names on a premiere model.</li>
+			<li>Model Name on Package: What the model is called on the specific package, This often differs by country and differs from the nam on the car itself.</li	
+		</ul>
+		
+		<h4>COLLECTIONS</h4>
+		<p>Every model you enter into your collection uses the UMID, VerID, VarID and optionally the RelID. In addition you can enter info on the purchase, value, storage location, date of purchase etc.</p>
+		
+		<h4>IDENTIFICATION SUMMARY</h4>
+		<p>Each specific model has a unique identifier that covers all of the info listed above. An example is:</p>
+		<ul>
+			<li>SF149-003-a-01: </li>
+			<p>This is model SF233 (Lesney Dodge Challenger, 3rd version of this (the blue one), first variation (chrome interior) and the 1st (and only) release of that variation- offered in Brazil in 1979.</p>
+			
+		</ul>
 	</div>
 </div>
 
