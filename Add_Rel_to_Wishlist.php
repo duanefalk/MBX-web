08db4fc9e076e1baafd589fe796827b10b4d3e9f
duncanfalk <djf@duncanfalk.com>
2015-02-23 15:49:15 -0500

    Add to Wishlist: page formatting

--- Add_Rel_to_Wishlist.php
+++ Add_Rel_to_Wishlist.php
@@ -60,28 +60,33 @@ session_start();
 		<?php
             $Rel_to_Add=$_GET["model"];
             $User="duanefalk";
-            echo "Release Selected: ".$Rel_to_Add."<br /><br />";
+            echo "<p><strong>Release Selected:</strong> ".$Rel_to_Add."</p>";
 
             $picture1= IMAGE_URL . $Rel_to_Add."_1.jpg";
 			$picture1_loc=IMAGE_PATH. $Rel_to_Add."_1.jpg";
 
             if (file_exists($picture1_loc)) {
-                echo "<img src=".$picture1." width=\"240\">";
+                echo "<img src=".$picture1." />";
             } else {
                 //no photo, echo DEFAULT_IMAGE;
-                echo "<img src=".DEFAULT_IMAGE." width=\"240\">";
+                echo "<img src=".DEFAULT_IMAGE." />";
             }
         ?>
-              
-		<form name="Add_Rel_to_Wishlist" action="Add_rel_to_Wishlist.php" method="post">
-			<input class="button dark" type="submit" value="<?php echo $Rel_to_Add?>" id="submit" name="rel_to_add" />
-        </form>
-
-		<?php
-		    $url= "Release_Detail.php?model=".$Rel_to_Add;
-		    echo "<a href=\"".$url."\">Cancel</a>";
-		?>
-
+        
+        <div class="row">
+	        <div class="large-6 columns">
+		        <form name="Add_Rel_to_Wishlist" action="Add_rel_to_Wishlist.php" method="post">
+					<input class="button dark" type="submit" value="<?php echo $Rel_to_Add?>" id="submit" name="rel_to_add" />
+		        </form>
+	        </div>
+	        <div class="large-6 columns">
+				<?php
+				    $url= "Release_Detail.php?model=".$Rel_to_Add;
+				    echo "<a href=\"".$url."\">Cancel</a>";
+				?>
+	        </div>
+        </div>
+        
 	</div>
 </div>
 
--- Add_Var_to_Wishlist.php
+++ Add_Var_to_Wishlist.php
@@ -9,81 +9,98 @@ session_start();
 <?php include("includes/header.php"); ?>
 <?php include("includes/functions.php"); ?>
 
-<?php
-    if (isset($_POST['var_to_add'])) {
-    	$User=$_SESSION['Username'];
-	$query3=("SELECT * FROM Matchbox_User_Collections WHERE Username='$User'");								
-	$result3=0;
-	$rows_count3=0;									
-	$result3 = mysql_query($query3);
-	$row3=mysql_fetch_array($result3);
-	$User_Coll_ID=$row3["User_Coll_ID"];
+<div class="row">
+	<div class="large-12 columns">
 
-	$VarID=$_POST["var_to_add"];
-        $VerID=substr($VarID,0,10);
-        $UMID=substr($VerID,0,6);        
+		<ul class="large-block-grid-2">
+			<li class="carGrid">
+	
+				<?php
+				    if (isset($_POST['var_to_add'])) {
+				    
+					    $User=$_SESSION['Username'];
+						$query3=("SELECT * FROM Matchbox_User_Collections WHERE Username='$User'");								
+						$result3=0;
+						$rows_count3=0;									
+						$result3 = mysql_query($query3);
+						$row3=mysql_fetch_array($result3);
+						$User_Coll_ID=$row3["User_Coll_ID"];
+					
+						$VarID=$_POST["var_to_add"];
+					    $VerID=substr($VarID,0,10);
+					    $UMID=substr($VerID,0,6);        
+					
+						$Wishlist_InactivFlg="0";
+				
+				
+				     
+				        $query="INSERT INTO Matchbox_User_Wishlist (Username, User_Coll_ID, UMID, VerID, VarID, Wishlist_InactivFlg) VALUES ('$User', '$User_Coll_ID', '$UMID','$VerID', '$VarID', '$Wishlist_InactivFlg')";
+						$outcome=mysql_query($query);
+				        
+				        if ($outcome) {
+				        	//if ((mysql_query($query)) == true)
+							echo "Added variation ".$VarID." to wishlist for ".$User." in collection ".$User_Coll_ID."<br />";
+				            echo "<p>Done and returning</p>";
+				            //require_once("includes/close_db_connection.php");
+				            //Return;
+				            $url= "Search_Models_Menu.php";
+							echo "<a href=\"".$url."\">Return to Search Models</a>";
+				            exit;
+				        } else {
+				            echo "<p>Subject creation failed. Please review entries.</p>";
+				            echo "<p>".mysql_error()."</p>";
+				            //drop down to form again
+				        }   
+				    }
+				//if post not set do initial form 
+				?>
+			
+				<h2>Add Variation to Wishlist</h2>
+		
+		        <?php
+		            $Var_to_Add=$_GET["model"];
+		            $User=$_SESSION['Username'];
+		
+		            echo "<p><strong>Variation Selected:</strong> ".$Var_to_Add."</p>";
+		
+		            $picture1= IMAGE_URL . $Var_to_Add."_1.jpg";
+				    $picture1_loc=IMAGE_PATH. $Var_to_Add."_1.jpg";
+		
+		            if (file_exists($picture1_loc)) {
+		                echo "<img src=".$picture1." />";
+		            } else {
+		                //no photo, echo DEFAULT_IMAGE;
+		                echo "<img src=".DEFAULT_IMAGE." />";
+		            } 
+				?>
+				
+				<div class="row">
+			        <div class="large-6 columns">
+				        <form name="Add_var_to_Wishlist" action="Add_Var_to_Wishlist.php" method="post">
+							<input class="button dark" type="submit" value="<?php echo $Var_to_Add?>" id="submit" name="var_to_add">
+				        </form>
+			        </div>
+			        <div class="large-6 columns">
+						<?php
+						    $url= "Variation_Detail.php?model=".$Var_to_Add;
+						    echo "<a href=\"".$url."\">Cancel</a>";
+						?>
+			        </div>
+		        </div>
+			</li>
+		</ul>
+	</div>
+</div>
 
-	$Wishlist_InactivFlg="0";
-
-
-     
-        $query="INSERT INTO Matchbox_User_Wishlist (Username, User_Coll_ID, UMID, VerID, VarID, Wishlist_InactivFlg) 
-            VALUES ('$User', '$User_Coll_ID', '$UMID','$VerID', '$VarID', '$Wishlist_InactivFlg')";
-   
-        $outcome=mysql_query($query);
-        if ($outcome) {
-        //if ((mysql_query($query)) == true)
-	    echo "Added variation ".$VarID." to wishlist for ".$User." in collection ".$User_Coll_ID."<br />";
-            echo "<p>Done and returning</p>";
-            //require_once("includes/close_db_connection.php");
-            //Return;
-            $url= "Search_Models_Menu.php";
-	    echo "<a href=\"".$url."\">Return to Search Models</a>";
-            exit;
-        } else {
-            echo "<p>Subject creation failed. Please review entries.</p>";
-            echo "<p>".mysql_error()."</p>";
-            //drop down to form again
-        }   
-    }
-//if post not set do initial form 
-?>
-
-<table id="structure">
-<tr>
-	<td id="navigation">
-                <a href="Search_Models_Menu.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Search Models</p></a>
-		<a href="Search_Releases_Menu.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Search Releases</p></a>
-                <a href="Update_Mdls_in_Coll.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Update Models in Your Collection</p></a>
-		<a href="index.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Main Page</p></a>			
-	</td>
-	<td id="page">
-		<h2>Add Variation to Wishlist</h2>
-		<br />
-                <?php
-                    $Var_to_Add=$_GET["model"];
-                    $User=$_SESSION['Username'];
-                    echo "Variation Selected: ".$Var_to_Add."<br /><br />";
-
-                    $picture1= IMAGE_URL . $Var_to_Add."_1.jpg";
-		    $picture1_loc=IMAGE_PATH. $Var_to_Add."_1.jpg";
-                    if (file_exists($picture1_loc)) {
-                        echo "<img src=".$picture1." width=\"240\">";
-                    } else {
-                        //no photo, echo DEFAULT_IMAGE;
-                        echo "<img src=".DEFAULT_IMAGE." width=\"240\">";
-                    }
-                ?>
-              
-		<form name="Add_var_to_Wishlist" action="Add_Var_to_Wishlist.php" method="post">
-		<input type="submit" value="<?php echo $Var_to_Add?>" id="submit" name="var_to_add">Add<br/>
-        	</form>
-
-                <?php
-		    $url= "Variation_Detail.php?model=".$Var_to_Add;
-		    echo "<a href=\"".$url."\">Cancel</a>";
-		?>
-            </td>
-	</tr>
-</table>
+	
+<!-- Sub Menu -->
+<div class="row" id="subNav">
+	<div class="large-12 columns">
+		<p class="tip">related pages:</p>
+		<a href="Search_Models.php">Search Models</a>
+		<a href="Search_Releases.php">Search Releases</a>
+		<a href="Update_Mdls_in_Coll.php">Update Models in Your Collection</a>
+		<a href="index.php">Return to Main Page</a>		
+	</div>
+</div>
 <?php include("includes/footer.php"); ?>
