675315a6f832f4388a6b4abbe28b07a83da3d77a
duncanfalk <djf@duncanfalk.com>
2015-03-26 15:38:01 -0400

    Add Rel to Wishlist formatting

--- Add_Rel_to_Wishlist.php
+++ Add_Rel_to_Wishlist.php
@@ -57,33 +57,37 @@ session_start();
 		
 		<h2>Add Release to Wishlist</h2>
 		
-		<?php
-            $Rel_to_Add=$_GET["model"];
-            $User="duanefalk";
-            echo "<p><strong>Release Selected:</strong> ".$Rel_to_Add."</p>";
-
-            $picture1= IMAGE_URL . $Rel_to_Add."_1.jpg";
-			$picture1_loc=IMAGE_PATH. $Rel_to_Add."_1.jpg";
-
-            if (file_exists($picture1_loc)) {
-                echo "<img src=".$picture1." />";
-            } else {
-                //no photo, echo DEFAULT_IMAGE;
-                echo "<img src=".DEFAULT_IMAGE." />";
-            }
-        ?>
+		<div class="row">
+			<div class="large-12 columns">
+				<?php
+		            $Rel_to_Add=$_GET["model"];
+		            $User="duanefalk";
+		            echo "<p><strong>Release Selected:</strong> " . $Rel_to_Add . "</p>";
+		
+		            $picture1 = IMAGE_URL . $Rel_to_Add . "_1.jpg";
+					$picture1_loc = IMAGE_PATH . $Rel_to_Add . "_1.jpg";
+		
+		            if (file_exists($picture1_loc)) {
+		                echo "<p><img src=".$picture1." /></p>";
+		            } else {
+		                //no photo, echo DEFAULT_IMAGE;
+		                echo "<p><img src=".DEFAULT_IMAGE." /></p>";
+		            }
+		        ?>
+			</div>
+		</div>
         
         <div class="row">
-	        <div class="large-6 columns">
+	        <div class="large-3 small-6 columns">
 		        <form name="Add_Rel_to_Wishlist" action="Add_rel_to_Wishlist.php" method="post">
 					<input class="button dark" type="submit" value="<?php echo $Rel_to_Add?>" id="submit" name="rel_to_add" />
 		        </form>
 	        </div>
-	        <div class="large-6 columns">
-				<?php
-				    $url= "Release_Detail.php?model=".$Rel_to_Add;
-				    echo "<a href=\"".$url."\">Cancel</a>";
+	        <div class="large-3 small-6 columns end">
+				<?php 
+					$url = "Release_Detail.php?model=" . $Rel_to_Add;
 				?>
+				<a class="button dark cancel" href="<?php $url; ?>">Cancel</a>				
 	        </div>
         </div>
         
