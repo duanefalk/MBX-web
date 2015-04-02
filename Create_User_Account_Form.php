b6aab2b242dbb74aa18e8f2c83882154bef7da4f
duanefalk <duanejfalk@yahoo.com>
2015-04-02 14:45:24 -0400

    cleanup format
    Used formrow from edit acct form here also

--- Create_User_Account_Form.php
+++ Create_User_Account_Form.php
@@ -18,77 +18,104 @@
 		<p><small>* required fields</small></p>
 		
 		<form action="Create_User_Account_Process.php" method="post">
-						
-			<label for="Username">Username* (4-20 chars, alphanumeric only):</label>
-			<input type="text" name="Username" value="" size="20" id="Username">
 			
-			<label for="Password">Password* (at least 4 chars, alphanumeric only):</label>
-			<input type="text" name="Password" value="" size="40" id="Password">
+			<div class="formRow">			
+				<label for="Username">Username* (4-20 chars, alphanumeric only):</label>
+				<input type="text" name="Username" value="" size="20" id="Username">
+			</div>
+			
+			<div class="formRow">
+				<label for="Password">Password* (at least 4 chars, alphanumeric only):</label>
+				<input type="text" name="Password" value="" size="40" id="Password">
+			</div>
 		
 			<?php $_SESSION['Sec_Lvl']=2; ?> 
 		
-			<label for="First_Name">First Name*:</label>
-			<input type="text" name="First_Name" value="" size="60" id="First_Name">
+			<div class="formRow">		
+				<label for="First_Name">First Name*:</label>
+				<input type="text" name="First_Name" value="" size="60" id="First_Name">
+			</div>
 			
-			<label for="Last_Name">Last Name*:</label>
-			<input type="text" name="Last_Name" value="" size="60" id="Last_Name">
+			<div class="formRow">
+				<label for="Last_Name">Last Name*:</label>
+				<input type="text" name="Last_Name" value="" size="60" id="Last_Name">
+			</div>
 			
-			<label for="User_Email">Email*:</label>
-			<input type="text" name="User_Email" value="" size="60" id="User_Email">
+			<div class="formRow">
+				<label for="User_Email">Email*:</label>
+				<input type="text" name="User_Email" value="" size="60" id="User_Email">
+			</div>
 			
-			<label for="User_Url">Home page url (if you have a web site):</label>
-			<input type="text" name="User_Url" value="" size="60" id="User_Url">
+			<div class="formRow">
+				<label for="User_Url">Home page url (if you have a web site):</label>
+				<input type="text" name="User_Url" value="" size="60" id="User_Url">
+			</div>
 			
-			<label for="User_Address">Address (please enter country at minimum; need full address to send special code 2 model to you):</label>
-			<input type="text" name="User_Address" value="" size="60" id="User_Address">
+			<div class="formRow">
+				<label for="User_Address">Address (please enter country at minimum; need full address to send special code 2 model to you):</label>
+				<input type="text" name="User_Address" value="" size="60" id="User_Address">
+			</div>
 			
-			<label for="User_Phone">Phone:</label>
-			<input type="text" name="User_Phone" value="" size="20" id="User_Phone">
+			<div class="formRow">
+				<label for="User_Phone">Phone:</label>
+				<input type="text" name="User_Phone" value="" size="20" id="User_Phone">
+			</div>
 			
-			<label for="ddlAreasOfInterest">Areas of Interest (hold down ctrl or cmd key to choose as many as apply):</label>
-			<?php
-				$query=("SELECT * FROM Matchbox_Value_Lists WHERE ValueList LIKE '%UserInterest%' ORDER BY ValueDispOrder ASC");								
-				$result=0;
-				$rows_count=0;									
-				$result = mysql_query($query);
-				if (!result) {
-					echo "Database query failed";
-				}
-				else {
-					//echo "made connection ".$result."<br />";		
-				}
-				$rows_count= mysql_num_rows($result);
-				// echo "Rows Count: ".$rows_count."<br />";
-				?>
-				<select multiple size="10" name="Areas_of_Interest[]" id="ddlAreasOfInterest">
-				<?php
-					for ($i=1; $i<=$rows_count; $i++) {
-						$row=mysql_fetch_array($result);
-						echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option'."<br />";
-					}	
-				?>
-				</select>
-				
-		<label for="User_is_Dealer">Are you a Matchbox dealer?:</label>     
-			<input type="radio" name="User_is_Dealer" id="User_is_Dealer1" value="0"> <label for="User_is_Dealer1">No</label><br>
-			<input type="radio" name="User_is_Dealer" id="User_is_Dealer2" value="1"> <label for="User_is_Dealer2">Yes</label><br>
+			<div class="formRow">
+				<label for="ddlAreasOfInterest">Areas of Interest (hold down ctrl or cmd key to choose as many as apply):</label>
+					<?php
+					$query=("SELECT * FROM Matchbox_Value_Lists WHERE ValueList LIKE '%UserInterest%' ORDER BY ValueDispOrder ASC");								
+					$result=0;
+					$rows_count=0;									
+					$result = mysql_query($query);
+					if (!result) {
+						echo "Database query failed";
+					}
+					else {
+						//echo "made connection ".$result."<br />";		
+					}
+					$rows_count= mysql_num_rows($result);
+					// echo "Rows Count: ".$rows_count."<br />";
+					?>
+					<select multiple size="10" name="Areas_of_Interest[]" id="ddlAreasOfInterest">
+					<?php
+						for ($i=1; $i<=$rows_count; $i++) {
+							$row=mysql_fetch_array($result);
+							echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option'."<br />";
+						}	
+					?>
+					</select>
+			</div>
 			
-		<label for="txtUser_Memberships">Matchbox clubs you belong to:</label>
-		<textarea name="User_Memberships" cols="45" rows="4" id="txtUser_Memberships"></textarea>
+			<div class="formRow">		
+				<label for="User_is_Dealer">Are you a Matchbox dealer?:</label>     
+				<input type="radio" name="User_is_Dealer" id="User_is_Dealer1" value="0"> <label for="User_is_Dealer1">No</label><br>
+				<input type="radio" name="User_is_Dealer" id="User_is_Dealer2" value="1"> <label for="User_is_Dealer2">Yes</label><br>
+			</div>
 			
-		<p>Preferred Scheme for Vehicle Condition</p>
-		<p>(alpha= MIB, Mint, Exc etc; num= 10, 9.5, 9 etc): </p>
-			<input type="radio" name="Veh_Cond_Scheme" id="Veh_Cond_Scheme1" value="0"> <label for="Veh_Cond_Scheme1">Alpha</label><br>
-			<input type="radio" name="Veh_Cond_Scheme" id="Veh_Cond_Scheme2" value="1"> <label for="Veh_Cond_Scheme2">Num</label><br>
+			<div class="formRow">	
+				<label for="txtUser_Memberships">Matchbox clubs you belong to:</label>
+				<textarea name="User_Memberships" cols="45" rows="4" id="txtUser_Memberships"></textarea>
+			</div>	
 			
-		<p>Preferred Scheme for Pkg Condition (as above):</p>
-			<input type="radio" name="Pkg_Cond_Scheme" id="Pkg_Cond_Scheme1" value="0"> <label for="Pkg_Cond_Scheme1">Alpha</label><br>
-			<input type="radio" name="Pkg_Cond_Scheme" id="Pkg_Cond_Scheme2" value="1"> <label for="Pkg_Cond_Scheme2">Num</label><br>
+			<div class="formRow">
+				<label for="">Preferred Scheme for Vehicle Condition <small>(alpha= MIB, Mint, Exc etc; num= 10, 9.5, 9 etc)</small></label>
+				<input type="radio" name="Veh_Cond_Scheme" id="Veh_Cond_Scheme1" value="0"> <label for="Veh_Cond_Scheme1">Alpha</label><br>
+				<input type="radio" name="Veh_Cond_Scheme" id="Veh_Cond_Scheme2" value="1"> <label for="Veh_Cond_Scheme2">Num</label><br>
+			</div>	
 		
-		<p>Code 2 Preference:</p>
-			<input type="radio" name="Code2_Pref" id="Code2_Pref1" value="0"> <label for="Code2_Pref1">Combine Code 1 and Code 2 versions in displays</label><br>
-			<input type="radio" name="Code2_Pref" id="Code2_Pref2" value="1"> <label for="Code2_Pref2">Show both Code 1 and Code 2, but separate them</label><br>
-			<input type="radio" name="Code2_Pref" id="Code2_Pref3" value="2"> <label for="Code2_Pref3">Don't show Code 2 at all</label><br>
+			<div class="formRow">
+				<label for="">Preferred Scheme for Pkg Condition <small>(as above)</small></label>
+				<input type="radio" name="Pkg_Cond_Scheme" id="Pkg_Cond_Scheme1" value="0"> <label for="Pkg_Cond_Scheme1">Alpha</label><br>
+				<input type="radio" name="Pkg_Cond_Scheme" id="Pkg_Cond_Scheme2" value="1"> <label for="Pkg_Cond_Scheme2">Num</label><br>
+			</div>
+			
+			<div class="formRow">
+				<label for="">Code 2 Preference</label>
+				<input type="radio" name="Code2_Pref" id="Code2_Pref1" value="0"> <label for="Code2_Pref1">Combine Code 1 and Code 2 versions in displays</label><br>
+				<input type="radio" name="Code2_Pref" id="Code2_Pref2" value="1"> <label for="Code2_Pref2">Show both Code 1 and Code 2, but separate them</label><br>
+				<input type="radio" name="Code2_Pref" id="Code2_Pref3" value="2"> <label for="Code2_Pref3">Don't show Code 2 at all</label><br>
+			</div>
 
 		<p>Upon selecting 'Submit' below, you will be sent to the login page. Please login under your new account.</p>	
 			
