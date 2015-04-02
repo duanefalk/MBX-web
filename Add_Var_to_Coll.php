a81b4d556c9f4829d0cf200113bd3697b37bed62
duanefalk <duanejfalk@yahoo.com>
2015-04-02 10:30:53 -0400

    Fixed typo in capital. of field name
    Should have fixe this function- now seems to bring up entire form

--- Add_Var_to_Coll.php
+++ Add_Var_to_Coll.php
@@ -164,7 +164,7 @@ if (isset($_POST['var_coll_submit'])) {
 		    $Veh_Cond_Scheme = "Num_cond";			
 		}
 			    
-                $query=("SELECT * FROM Matchbox_Value_lists WHERE ValueList LIKE '%$Veh_Cond_Scheme%' ORDER BY ValueDispOrder ASC");								
+                $query=("SELECT * FROM Matchbox_Value_Lists WHERE ValueList LIKE '%$Veh_Cond_Scheme%' ORDER BY ValueDispOrder ASC");								
                 $result=0;
                 $rows_count=0;									
                 $result = mysql_query($query);
