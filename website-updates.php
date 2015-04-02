e4455c1a963bebd8f3d8c8ad9c0c963801ea3e66
duncanfalk <djf@duncanfalk.com>
2015-04-02 15:15:59 -0400

    new page: Website Updates

---
+++ website-updates.php
@@ -0,0 +1,50 @@
+<?php 	
+	$pageTitle = "Website Updates";
+	include("includes/header.php");
+?>
+
+<div class="row">
+	<div class="large-12 columns">
+	
+		<h2>Latest Website Updates</h2>
+			
+		<div class="section">
+			<p class="date">April 2, 2015</p>
+			<ul>
+				<li>Added Status Update function</li>
+				<li>Fixed 'Create Account': on successful account creation it sends you directly to login page (fixes the 'Hello ___' problem in header)</li>
+				<li>Fixed 'Edit Account' to show previous field entries</li>
+				<li>New Model: Ford Mini Pickup (SF0242)</li>
+				<li>New Model: Audi Quattro (SF0243)</li>
+				<li>New Model: Volvo trucks (cable truck, zoo truck, container truck) (SF0244)</li>
+			</ul>
+		</div>
+		
+		<!--div class="section">
+			<p class="date">April 2, 2015</p>
+			<ul>
+				<li>Added Status Update function</li>
+				<li>Fixed 'Create Account': on successful account creation it sends you directly to login page (fixes the 'Hello ___' problem in header)</li>
+				<li>Fixed 'Edit Account' to show previous field entries</li>
+				<li>New Model: Ford Mini Pickup (SF0242)</li>
+				<li>New Model: Audi Quattro (SF0243)</li>
+				<li>New Model: Volvo trucks (cable truck, zoo truck, container truck) (SF0244)</li>
+			</ul>
+		</div-->
+						
+	</div>
+</div>
+
+<!-- Sub Menu -->
+<div class="row" id="subNav">
+	<div class="large-12 columns">
+		<p class="tip">related pages:</p>
+		<a href="DB_explain.php">About the Database</a>
+		<a href="MB_Code_System.php">The Code System</a>
+		<a href="About_photos.php">Links & Photo References</a>
+		<a href="Ref_files.php">Reference Files</a>
+		<a href="Dedication.php">Dedication</a>
+	</div>
+</div>
+
+<?php include("includes/footer.php"); ?>
