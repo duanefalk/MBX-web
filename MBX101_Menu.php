443236df1cd38d7a288be877c6e2886c215874c9
duanefalk <duanejfalk@yahoo.com>
2015-04-03 13:34:29 -0400

    add mbx101 menu page
    Need link form main page

---
+++ MBX101_Menu.php
@@ -0,0 +1,47 @@
+<?php include("includes/header.php"); ?>
+
+<div class="row">
+	<div class="large-12 columns">
+		<h2>Matchbox 101- Learn About Matchbox and Diecast Cars!</h2>
+		<p>Select from the following 'core curriculum' for informational articles on Matchbox topics.</p>
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-12 columns">
+		<h4>General Diecast Topics</h4>
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-12 columns">
+        <a href="How_Made.php" class="button dark">How are Diecast Cars Made?</a>
+        <a href="Diecast_History.php" class="button dark">Short History of Diecast Toys</a>	
+	<a href="Diecast_Brands.php" class="button dark">A Plethora of Diecast Car Brands</a>
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-12 columns">
+		<h4>Introduction to Matchbox</h4>
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-12 columns">
+        <a href="Window_Colors.php" class="button dark">A Brief History of the 'Matchbox' Brand</a>
+        <a href="Wheel_Types.php" class="button dark">Early Lesney Toys</a>	
+	<a href="Theme_List.php" class="button dark">What's the Difference? Regular & Superfast models</a>
+	<a href="Series_List.php" class="button dark">Ways of Organizing Matchbox Models</a>	
+	</div>
+</div>
+
+<!-- Sub Menu -->
+<div class="row" id="subNav">
+	<div class="large-12 columns">
+		<p class="tip">related pages:</p>
+		<a href="About_site.php">About the Site</a>
+	</div>
+</div>
+
+<?php include("includes/footer.php"); ?>
