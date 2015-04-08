2814b75caa146d9b2995616ad8cd4cfa8ba68f1f
duncanfalk <djf@duncanfalk.com>
2015-04-08 08:40:39 -0400

    Header: adding MBX 101 to menu

--- includes/header.php
+++ includes/header.php
@@ -43,22 +43,22 @@ include_once("includes/analyticstracking.php");
 		<?php include("includes/navUtility.php"); ?>
 		
 		<div class="row">
-			<div class="large-4 columns">
+			<div class="large-3 columns">
 				<a href="index.php" id="logo"><img src="images/logo.png" width="225" height="59" /></a>
 			</div>
 			
-			<div class="large-8 columns">
+			<div class="large-9 columns">
 				<ul id="mainNav">
 					<li><a href="About_site.php">About</a></li>
 					<li><a href="Search_Models_Menu.php">Search Models</a></li>
-					<li><a href="Search_Releases_Menu.php">Search Releases</a></li>
-					
+					<li><a href="Search_Releases_Menu.php">Search Releases</a></li>					
 					<?php 
 						if ($_SESSION['Sec_Lvl'] > 1) {
 							echo "<li><a href='Collections_Menu.php'>Your Collections</a></li>";
 							echo "<li><a href='User_Upload.php'>Upload</a></li>";
 						}
-					?>
+					?>					
+					<li><a href="MBX101_Menu.php">MBX 101</a></li>
 				</ul>
 			</div>
 		</div>
