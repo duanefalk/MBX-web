2d637396667269aaa4cb40cc0cad34d53f97ee61
duncanfalk <djf@duncanfalk.com>
2015-05-01 11:55:55 -0400

    Update User Collection Code Process: form updates

--- Updt_User_Coll_Code_Process.php
+++ Updt_User_Coll_Code_Process.php
@@ -7,6 +7,10 @@ session_start();
 <?php include("includes/header.php"); ?>
 <?php include("includes/functions.php"); ?>
 
+
+<div class="row">
+	<div class="large-12 columns">
+
 <?php
     if (isset($_POST['var_coll_code_submit'])) {
 
@@ -37,25 +41,21 @@ session_start();
             //drop down to form again
         }   
     } else {
-//if post not set do initial form 
+	//if post not set do initial form 
 ?>
 
-<table id="structure">
-<tr>
-
-	<td id="page">
-
-		<h2>Update/Delete Seller or Location Codes</h2>
-
-		<br />
-		<?php
-                    $User=$_SESSION['Username'];
-		    $Code_to_Updt=$_GET["code"];
-		    $query=("SELECT * FROM Matchbox_User_Coll_Value_Lists WHERE Username='$User' AND Coll_List_Value LIKE'%$Code_to_Updt%'");								
-		    $result=0;
-		    $result=mysql_query($query);
-		    if ($result) {
+	<h2>Update/Delete Seller or Location Codes</h2>
+	
+	<?php
+		$User=$_SESSION['Username'];
+		$Code_to_Updt=$_GET["code"];
+		$query=("SELECT * FROM Matchbox_User_Coll_Value_Lists WHERE Username='$User' AND Coll_List_Value LIKE'%$Code_to_Updt%'");								
+		$result=0;
+		$result=mysql_query($query);
+		
+		if ($result) {
 			$rows=mysql_num_rows($result);
+		
 			if ($rows > 0) {
 			    $row=mysql_fetch_array($result);
 			    $Code_Type=$row['Coll_List_Type'];
@@ -63,33 +63,45 @@ session_start();
 			    echo "You don't have that code.";
 			    exit;
 			}
-		    }
-                ?>
+		}
+    ?>
 
-		<form name="Updt_User_Coll_Code_Process" action="Updt_User_Coll_Code_Process.php" method="post">
-		    <?php  
-                        echo "<br />";
-			echo "Username: ".$User."<br />";
-			echo "Code to Updt/delete: ".$Code_to_Updt."<br />";
-			
-                    ?>
-		    <input type="hidden" name="Code_to_Updt" value="<?php echo $Code_to_Updt;?>" id="Code_to_Updt">
-		    <p>Code Type (either Location or Seller):          <input type="text" name="Coll_List_Type" value="<?php echo $Code_Type;?>" size="60" id="Coll_List_Type"></p>
-		    <p>Code Display Order (1-4 digit#):          <input type="text" name="Coll_List_Val_DisplOrd" value="<?php echo $row["Coll_List_Val_DisplOrd"];?>" size="4" id="Coll_List_Val_DisplOrd"></p>
-
-                    <input type="submit" name="var_coll_code_submit" class="button dark" value="Update" id="var_coll_code_submit"/>
-        	</form>
-                <?php
-		    $url1= "Del_Coll_Code.php?code=".$Code_to_Updt;
-		    echo "<a href=\"".$url1."\">DELETE THIS CODE</a>";
-		    echo "<br></><br></>";
-		    $url2= "Updt_User_Coll_Code.php";
-		    echo "<a href=\"".$url2."\">Cancel</a>";
+	<form name="Updt_User_Coll_Code_Process" action="Updt_User_Coll_Code_Process.php" method="post" data-parsley-validate>
+	    <?php  
+        	echo "<p><strong>Username:</strong> ".$User."</p>";
+			echo "<p><strong>Code to Updt/delete:</strong> ".$Code_to_Updt."</p>";
 		?>
-            </td>
-	</tr>
-</table>
+	    
+	    <input type="hidden" name="Code_to_Updt" value="<?php echo $Code_to_Updt;?>" id="Code_to_Updt">
+	    
+	    <div class="formRow">
+		    <label for="Coll_List_Type">Code Type (either Location or Seller):</label>
+		    <select name="Coll_List_Type" id="Coll_List_Type">
+				<option value="Location" <?php echo $Code_Type;?> <?php if ($Code_Type == "Location") { echo "selected"; } ?>>Location</option>
+				<option value="Seller" <?php if ($Code_Type == "Seller") { echo "selected"; } ?>>Seller</option>
+			</select>	
+			<!--input type="text" name="Coll_List_Type" id="Coll_List_Type" value="<?php echo $Code_Type;?>" size="60" id="Coll_List_Type"-->
+	    </div>
+	    
+	    <div class="formRow">
+		    <label for="Coll_List_Val_DisplOrd">Code Display Order (1-4 digit#):</label>
+		    <input type="text" name="Coll_List_Val_DisplOrd" id="Coll_List_Val_DisplOrd" value="<?php echo $row["Coll_List_Val_DisplOrd"];?>" size="4" id="Coll_List_Val_DisplOrd" data-parsley-type="integer">
+	    </div>
+	    
+		<input type="submit" name="var_coll_code_submit" class="button dark" value="Update" id="var_coll_code_submit"/>
+    </form>
+    
+    <?php
+	    $url1= "Del_Coll_Code.php?code=".$Code_to_Updt;
+	    echo "<a href=\"".$url1."\">DELETE THIS CODE</a>";
+	    echo "<br></><br></>";
+	    $url2= "Updt_User_Coll_Code.php";
+	    echo "<a href=\"".$url2."\">Cancel</a>";
+	?>
 <?php } ?>
+
+	</div>
+</div>
 
 <!-- Sub Menu -->
 <div class="row" id="subNav">
