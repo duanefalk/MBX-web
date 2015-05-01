a182c1f6bdf0f0ae134e89c5414e5bab84b4c45d
duanefalk <duanejfalk@yahoo.com>
2015-05-01 13:10:08 -0400

    website-updates
    added latest stuff as of 5/02

--- website-updates.php
+++ website-updates.php
@@ -10,13 +10,31 @@
 		<h2>Latest Website Updates</h2>
 			
 		<div class="section">
+			<p class="date">May 02, 2015</p>
+			<ul>
+				<li>FIXES: Lots of work! Thanks to all 'beta testers' who found bugs for me.</li>
+				<li>Fixed issues with creating collection. Can now create collection without system error. Coll name limited to 12 chars</li>
+				<li>Variation and Release detail screens: fixed bug that allowed you to add models to collection or wishlist even if you had not created a collection, or didn't have appropriate security (ie not logged in). Now fixed: will not see collection options on these screens unless you are logged in and have created a collection first.</li>
+				<li>Added validation to many user input fields (ie collection code display order now requires a number)</li>
+				<li>Fixed issues on Manage Collections screen- now shows appropriate check or x for steps you've completed or not</li>
+				<li>Open Items: Collection Summary Reports are not retuning corect results. Will work on those over next coupel of days. Models in Collection reports does work.</li>
+				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0229">BMW M1 (SF0229)</a></li>
+				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0230">Snorkel Fire Engine (SF0230)</a></li>
+				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0231">Ferrari 308 GTB (SF0231)</a></li>
+				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0232">Leyland Articulated Truck Cab (SF0232)</a></li>
+				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0233">Dodge Challenger (SF0233)</a></li>
+				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0234">4x4 Jeep (SF0234)</a></li>
+			</ul>
+		</div>
+		
+		<div class="section">
 			<p class="date">April 22, 2015</p>
 			<ul>
 				<li>ISSUE RESOLUTION: The problem with uploading files has been corrected. Uploaded files are now received by the site administrator, and the database record for the upload is created correctly.</li>	
 				<li>Added Status Update function</li>
 				<li>Fixed 'Create Account': on successful account creation it sends you directly to login page (fixes the 'Hello ___' problem in header)</li>
 				<li>Fixed 'Edit Account' to show previous field entries</li>
-				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0235">Ford Mini Pickup (SF0235)</a></li>
+				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0235">VW Rompin Rabbit (SF0235)</a></li>
 				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0236">Ford Mini Pickup (SF0236)</a></li>
 				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0237">Pontiac Firebird SE (SF0237)</a></li>
 				<li>New Model: <a href="<?php ROOTURL; ?>Models_Detail_and_Ver_Listing.php?model=SF0238">Ford Open Back Truck (SF0238)</a></li>
