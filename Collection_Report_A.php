4468dd120ac861388fdb43b52479726d276803e9
duanefalk <duanejfalk@yahoo.com>
2015-05-04 14:59:45 -0400

    Coll rep A and H
    Fixed coll report A, and added User selection to H. Pushing these

--- Collection_Report_A.php
+++ Collection_Report_A.php
@@ -16,35 +16,113 @@ session_start();
 		$Username=$_SESSION['Username'];
 		$result = mysql_query("SELECT * FROM Matchbox_User_Collections WHERE Username='$Username'");
 		$row=mysql_fetch_array($result);
-		echo "<h5>User: ".$Username."</h5>";
-		echo "<h5>Collection Name: ".$row['User_Coll_ID']."</h5>";
-		echo "<h5>Description: ".$row['User_Coll_Desc']."</h5>";
-		echo "<h5>Created on: ".$row['User_Coll_Created_Date']."</h5>";
+		echo "<h6>User: ".$Username."</h6>";
+		echo "<h6>Collection Name: ".$row['User_Coll_ID']."</h6>";
+		echo "<h6>Description: ".$row['User_Coll_Desc']."</h6>";
+		echo "<h6>Created on: ".$row['User_Coll_Created_Date']."</h6>";
 	    
+	    
+		//collection code info
 		echo "<table border='1' style='border-collapse: collapse;border-color: silver;'>";  
-		echo "<tr style='font-weight: bold;'>";  
-		echo "<td width='200' align='center'># of Sellers Defined</td>";  
-		echo "<td width='200' align='center'># of Locations Defined</td>";
-		echo "</tr>";
-		
-		echo "<tr style='font-weight: bold;'>";
-		$result2 = mysql_query("SELECT COUNT(DISTINCT(Coll_List_Value)) FROM Matchbox_User_Coll_Value_List WHERE Username='$Username' AND Coll_List_Type='Seller'"); 		
-		if ($result2) {
-			$row2 = mysql_fetch_array($result2);			 
-			echo "<td align='center' width='200'>" . $row2['COUNT(DISTINCT(Coll_List_Value)'] . "</td>";
-		} else {
-			echo "<td align='center' width='200'>" . "no sellers defined</td>";
-		}
-		$result3 = mysql_query("SELECT COUNT(DISTINCT(Coll_List_Value)) FROM Matchbox_User_Coll_Value_List WHERE Username='$Username' AND Coll_List_Type='Location'");
-		if ($result3) {
-			$row3 = mysql_fetch_array($result3);			 
-			echo "<td align='center' width='200'>" . $row3['COUNT(DISTINCT(Coll_List_Value)'] . "</td>";
-		} else {
-			echo "<td align='center' width='200'>" . "no locations defined</td>";
-		}
-		echo "</tr>";
+			echo "<tr style='font-weight: bold;'>";
+				echo "<td width='200' align='center'>Sellers Defined</td>";  
+			echo "</tr>";
+			
+			$result2=mysql_query("SELECT DISTINCT Coll_List_Value FROM Matchbox_User_Coll_Value_Lists
+			     WHERE Username='$Username' AND Coll_List_Type LIKE '%Seller%' ORDER BY Coll_List_Val_DisplOrd ASC");
+				
+				//$result2 = mysql_query("SELECT Coll_List_Value FROM Matchbox_User_Coll_Value_Lists WHERE Username='$Username' AND Coll_List_Type='Seller'"); 		
+			if ($result2) {
+				$rows_count2=mysql_num_rows($result2);
+				    for ($i=1; $i<=$rows_count2; $i++) {
+					echo "<tr style='font-weight: normal;'>";
+					$row2=mysql_fetch_array($result2);
+					echo "<td align='center' width='200'>" . $row2['Coll_List_Value'] . "</td>";
+				    }
+			} else {
+				echo "<td align='center' width='200'>" . "no sellers defined</td>";
+			}
+				
 		echo "</table>";
+		
+		echo "<table border='1' style='border-collapse: collapse;border-color: silver;'>";  
+			echo "<tr style='font-weight: bold;'>";
+				echo "<td width='200' align='center'>Locations Defined</td>";  
+			echo "</tr>";
+				
+			$result3=mysql_query("SELECT DISTINCT Coll_List_Value FROM Matchbox_User_Coll_Value_Lists
+						     WHERE Username='$Username' AND Coll_List_Type LIKE '%Location%' ORDER BY Coll_List_Val_DisplOrd ASC");
+			
+			if ($result3) {
+				$rows_count3=mysql_num_rows($result3);
+				    for ($i=1; $i<=$rows_count3; $i++) {
+					echo "<tr style='font-weight: normal;'>";
+					$row3=mysql_fetch_array($result3);
+					echo "<td align='center' width='200'>" . $row3['Coll_List_Value'] . "</td>";
+				    }
+			} else {
+				echo "<td align='center' width='200'>" . "no sellers defined</td>";
+			}
+		echo "</table>";
+		
+		
+		//Gather Collection record info
+		$TotPurchPrice=0;
+		$TotValue=0;
+		$TotSellFlg=0;
+		$MinSell=0;
+		
+		$result4=mysql_query("SELECT * FROM Matchbox_Collection WHERE Username='$Username'");
+		if ($result4) {
+			$rows_count4=mysql_num_rows($result4);
+			for ($i=1; $i<=$rows_count4; $i++) {
+				$row4=mysql_fetch_array($result4);
+				$TotPurchPrice=$TotPurch_Price + $row4['PurchPrice'];
+				$TotValue=$TotValue + $row4['ItemVal'];
+				if ($row4['SellFlag']) {
+					$TotSellFlg= $TotSellFlg +1;
+					$MinSell= $MinSell + $row4['MinSellPrice'];
+				}
+			}	
+
+			//Display tables w/ collection info
+			echo "<table border='1' style='border-collapse: collapse;border-color: silver;'>";  
+				echo "<tr style='font-weight: bold;'>";
+					echo "<td width='200' align='center'># Model Records</td>";  
+				echo "</tr>";
+				echo "<tr style='font-weight: normal;'>";
+					echo "<td align='center' width='200'>" . $rows_count4 . "</td>";
+				echo "</tr>";
+			echo "</table>";		
+
+			echo "<table border='1' style='border-collapse: collapse;border-color: silver;'>";  
+				echo "<tr style='font-weight: bold;'>";
+					echo "<td width='200' align='center'>Tot. Purchase Cost</td>";
+					echo "<td width='200' align='center'>Tot. Value</td>";	
+				echo "</tr>";
+
+				echo "<tr style='font-weight: normal;'>";
+					echo "<td align='center' width='200'>" . $TotPurchPrice . "</td>";
+					echo "<td align='center' width='200'>" . $TotValue . "</td>";
+				echo "</tr>";
+			echo "</table>";	
+
+			echo "<table border='1' style='border-collapse: collapse;border-color: silver;'>";  
+				echo "<tr style='font-weight: bold;'>";	
+					echo "<td width='200' align='center'># Flagged to Sell</td>";
+					echo "<td width='200' align='center'>Tot. Min. Sell Amt.</td>";
+				echo "</tr>";
+				echo "<tr style='font-weight: normal;'>";
+					echo "<td align='center' width='200'>" . $TotSellFlg . "</td>";
+					echo "<td align='center' width='200'>" . $MinSell . "</td>";
+				echo "</tr>";
+			echo "</table>";	
+		} else {
+			echo "There is a database error- please contact sys admin";
+		}		
+	
             ?>
+	    <a id="printThis" class="button dark" href="javascript:window.print()">Print this Report</a>
 	</div>
 </div>
 
--- Collection_Report_H.php
+++ Collection_Report_H.php
@@ -11,17 +11,18 @@ session_start();
 	<div class="large-12 columns">
             <a href="Collection_Reports.php" class="button dark">Return to Collection Reports Menu</a>
             <a href="index.php" class="button dark">Return to Main Page</a>
-            <?php  
+            <?php
+		$User=$_SESSION['Username'];
                 echo "<h2>Seller Summary </h2>";  
                 echo "<table border='1' style='border-collapse: collapse;border-color: silver;'>";  
                 echo "<tr style='font-weight: bold;'>";  
-                echo "<td width='200' align='center'>Country</td>";  
+                echo "<td width='200' align='center'>Seller</td>";  
                 echo "<td width='200' align='center'>Number of models</td>";
                 echo "<td width='200' align='center'>Total $ Spent</td>";
                 echo "</tr>";  
                 $result = mysql_query("SELECT Seller,COUNT(*),SUM(PurchPrice)  
-                FROM Matchbox_Collection  
-                GROUP BY Seller;");  
+                FROM Matchbox_Collection WHERE Username='$User'
+                GROUP BY Seller ASC;");  
                 while($row=mysql_fetch_array($result))  
                 {  
                     echo "<tr>";  
