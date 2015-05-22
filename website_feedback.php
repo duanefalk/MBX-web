71dbec2d27ef7a2440a620de0edc7d7c96f4fdce
duncanfalk <djf@duncanfalk.com>
2015-05-22 11:40:01 -0400

    Website Feedback (Work in Progress)

---
+++ website_feedback.php
@@ -0,0 +1,34 @@
+<?php 
+	$pageTitle = "About";
+	include("includes/header.php");
+?>
+
+
+<div class="row">
+	<div class="large-12 columns">
+	
+		<h2>Website Feedback</h2>
+		<p>replace</p>	
+		
+		<!--form action="<?php ROOTURL; ?>/includes/mail.php" method="POST">
+			
+			[still working on this] : http://www.1stwebdesigner.com/custom-php-contact-forms/
+			
+		</form-->
+						
+	</div>
+</div>
+
+<!-- Sub Menu -->
+<div class="row" id="subNav">
+	<div class="large-12 columns">
+		<p class="tip">related pages:</p>
+		<a href="DB_explain.php">About the Database</a>
+		<a href="MB_Code_System.php">The Code System</a>
+		<a href="About_photos.php">Links & Photo References</a>
+		<a href="Ref_files.php">Reference Files</a>
+		<a href="Dedication.php">Dedication</a>
+	</div>
+</div>
+
+<?php include("includes/footer.php"); ?>
