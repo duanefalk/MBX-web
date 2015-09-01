fc435eea6f270f9a363a4f9bb08854b1bf578774
Duncan Falk <djf@duncanfalk.com>
2015-09-01 13:41:36 -0400

    Older Browser Support: added Polyfills

--- includes/header.php
+++ includes/header.php
@@ -29,7 +29,18 @@
 	<link href="stylesheets/interior.css" media="all" rel="stylesheet" type="text/css" />
 	<link href="stylesheets/print.css" media="all" rel="stylesheet" type="text/css" />
 	
-	<script src="js/vendor/modernizr.js"></script>	
+	<script src="js/vendor/modernizr.js"></script>
+	
+	<!-- IE8 Polyfills -->
+	<!--[if lt IE 9]>
+	    <link rel="stylesheet" type="text/css" href="stylesheets/ie8.css" />
+	<![endif]-->
+	<!--[if lt IE 9]>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+		<script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
+		<script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+	<![endif]-->
 </head>
 
 <?php
