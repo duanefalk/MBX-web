94e13dc081641b64cab680d6ff53ea521738568f
Duncan Falk <djf@duncanfalk.com>
2015-09-02 21:08:06 -0400

    iPhone copy/paste bug

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
 <html class="no-js" lang="en">
 
 <head>
-	<meta charset="utf-8" />
+	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
 	<meta name="description" content="Matchbox Cars Database and Collections">
 	<meta name="keywords" content="Matchbox,Matchbox cars,Superfast,Regular Wheels,Matchbox packages,Matchbox wheels">
