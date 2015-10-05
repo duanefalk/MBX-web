432e55bb156ef64f5fc73f3300d199b0c6d91d7e
Duncan Falk <djf@duncanfalk.com>
2015-10-05 16:52:18 -0400

    Add Reference: formatting

--- Add_Reference_Form.php
+++ Add_Reference_Form.php
@@ -1,59 +1,72 @@
 <?php require_once("includes/db_connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
 <?php include("includes/header.php"); ?>
-<table id="structure">
-				<tr>
-					<td id="navigation">
-						<a href="Add_Menu.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Add New Record</p></a>
-						<a href="index.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Main Page</p></a>			
-					</td>
-					<td id="page">
-						<h2>Add a Reference</h2>
-						<p></p>
 
-				<!-- fields:
-				RefType
-				RefCode
-				RefName
-				RefDetails
-				RefComment
-				-->		
+<div class="row">
+	<div class="large-12 columns">
+		<h2>Add a Reference</h2>
+		
+		<!-- fields:
+			RefType
+			RefCode
+			RefName
+			RefDetails
+			RefComment
+		-->		
 				
 		<form action="Add_Reference_Process.php" method="post">
-			<p>Reference Code:     	  <input type="text" name="RefCode" value="" size="8" id="RefCode"</p>
-			<p>Reference Type:     	</p>
-				<?php
-					$query=("SELECT * FROM Matchbox_Value_Lists WHERE ValueList LIKE '%ReferenceType%'");								
-					$result=0;
-					$rows_count=0;									
-					$result = mysql_query($query);
-					if (!result) {
-						echo "Database query failed";
-					}
-					else {
-						//echo "made connection ".$result."<br />";		
-					}
-					$rows_count= mysql_num_rows($result);
-					// echo "Rows Count: ".$rows_count."<br />";
-				?>
-				<select name="RefType">
-				<?php
-					for ($i=1; $i<=$rows_count; $i++) {
-						$row=mysql_fetch_array($result);
-						echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option'."<br />";
-					}	
-				?>
-				</select>
-			<p>Name of Reference:          <input type="text" name="RefName" value="" size="30" id="RefName"</p>
-			<p>Reference Details (i.e. url, book title):    <input type="text" name="RefDetails" value="" size="40" id="RefDetails"</p>
-			<p>Reference Comment:     	</p>
-				<textarea name="RefComment" cols="45" rows="4">			
-				</textarea>
+			
+			<label for="RefCode">Reference Code:</label>
+			<input type="text" name="RefCode" value="" size="8" id="RefCode">
+			
+			<label for="RefType">Reference Type:</label>
+			<?php
+				$query=("SELECT * FROM Matchbox_Value_Lists WHERE ValueList LIKE '%ReferenceType%'");								
+				$result=0;
+				$rows_count=0;									
+				$result = mysql_query($query);
+				if (!result) {
+					echo "Database query failed";
+				} else {}
+				
+				$rows_count= mysql_num_rows($result);
+			?>
+			<select name="RefType" id="RefType">
+			<?php
+				for ($i=1; $i<=$rows_count; $i++) {
+					$row=mysql_fetch_array($result);
+					echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option>';
+				}	
+			?>
+			</select>
+			
+			<label for="RefName">Name of Reference:</label>
+			<input type="text" name="RefName" value="" size="30" id="RefName">
+			
+			<label for="RefDetails">Reference Details (i.e. url, book title):</label>
+			<input type="text" name="RefDetails" value="" size="40" id="RefDetails">
+			
+			<label for="RefComment">Reference Comment:</label>
+			<textarea name="RefComment" id="RefComment" cols="45" rows="4"></textarea>
 
-			<input type="submit" name="submit" value="Submit"/>
-		</form>			
-		<a href="Add_Reference_Form.php">Cancel</a>						 						
-	</td>
-</tr>
-</table>
+			<div class="row">
+				<div class="large-3 medium-6 small-6 columns">
+					<input class="button dark" type="submit" name="submit" value="Submit"/>
+				</div>
+				<div class="large-3 medium-6 small-6 columns end">
+					<a class="button dark cancel" href="Add_Reference_Form.php">Cancel</a>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+
+<!-- Sub Menu -->
+<div class="row" id="subNav">
+	<div class="large-12 columns">
+		<p class="tip">related pages:</p>
+		<a href="Add_Menu.php">Add New Record</a>
+	</div>
+</div>
+
 <?php require("includes/footer.php"); ?>						 						
