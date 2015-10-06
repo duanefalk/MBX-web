15d4d866c2d290f2f7206b24fad2e732fc948260
Duncan Falk <djf@duncanfalk.com>
2015-10-06 10:26:17 -0400

    User Signup: adding address to email notification

--- Create_User_Account_Process.php
+++ Create_User_Account_Process.php
@@ -68,7 +68,7 @@ session_start();
         //Mail inputs
 	    $emails = "info@mbx-u.com,duanejfalk@yahoo.com";
 	    $subject = "MBX-U: New Member!";
-	    $message = "We have a new member of Matchbox University named " . $First_Name . " " . $Last_Name . ".\r\nTheir email is " . $User_Email . ".";
+	    $message = "We have a new member of Matchbox University named " . $First_Name . " " . $Last_Name . ".\r\n\r\nTheir email is " . $User_Email . ".\r\nTheir address is " . $User_Address . ".";
 	    $headers = 'From:info@mbx-u.com' . "\r\n" . 'Reply-To:info@mbx-u.com';
 	    
         mail($emails,$subject,$message,$headers);
