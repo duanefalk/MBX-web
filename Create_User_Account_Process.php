14c81f898b66f61c7d7470b5653eeac7a2af5163
Duncan Falk <djf@duncanfalk.com>
2015-10-13 09:29:33 -0400

    Create_User_Account

--- Create_User_Account_Process.php
+++ Create_User_Account_Process.php
@@ -34,15 +34,16 @@ session_start();
     $User_Address=mysql_real_escape_string($_POST['User_Address']);
     $User_Phone=mysql_real_escape_string($_POST['User_Phone']);
     $Areas_of_Interest=$_POST['Areas_of_Interest'];
-        if(isset($Areas_of_Interest)) {
-            $InterestString='';
-            foreach ($Areas_of_Interest as $key => $value) { 
-                $InterestString .= $value.", "; 
-            }}
-        else
-            {
-              $InterestString='';
-            }
+
+    if (isset($Areas_of_Interest)) {
+        $InterestString='';
+        foreach ($Areas_of_Interest as $key => $value) { 
+            $InterestString .= $value.", "; 
+		}
+	}
+    else {
+        $InterestString='';
+    }
   
     $User_is_Dealer=$_POST['User_is_Dealer'];
     $User_Memberships=mysql_real_escape_string($_POST['User_Memberships']);
@@ -57,6 +58,7 @@ session_start();
             VALUES ('$Username','$Password','$Sec_Lvl','$First_Name','$Last_Name', '$User_Email','$User_Url',
             '$User_Address','$User_Phone', ' $InterestString','$User_is_Dealer', '$User_Memberships',
             '$User_Veh_Cond_Scheme', '$User_Pkg_Cond_Scheme', '$User_Code2_Pref')";
+   
     // mysql_query($query);
     // "turned output buffering on cause this was giving me errors- couldnt find the source in the includes;
     // header("Location: Add_Version.php");
