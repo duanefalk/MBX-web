1f618d2feea4ba35e2cfcc96bea51279d734fbc5
Duncan Falk <djf@duncanfalk.com>
2015-11-16 12:37:37 -0500

    Formatting Admin pages

--- Add_Model_Form.php
+++ Add_Model_Form.php
@@ -8,141 +8,156 @@
 		<form action="Add_Model_Process.php" method="post">
 			<label for="UMID">UMID:</label>
 			<input type="text" name="UMID" value="" size="6" id="UMID">
+			
 			<label for="MasterModelName">Master Model Name: </label>
 			<input type="text" name="MasterModelName" value="" size="60" id="MasterModelName">
+			
 			<label for="ModelPhotoName">Model Photo Name: </label>	
 			<input type="text" name="ModelPhotoName" value="" size="20" id="ModelPhotoName">
+			
 			<label for="ModelPhotoRef">Model Photo Reference:</label>	
-				<?php
-					$query=("SELECT * FROM Matchbox_References ORDER BY RefCode ASC");								
-					$result=0;
-					$rows_count=0;									
-					$result = mysql_query($query);
-					if (!result) {
-						echo "Database query failed";
-					}
-					else {
-						//echo "made connection ".$result."<br />";		
-					}
-					$rows_count= mysql_num_rows($result);
-					// echo "Rows Count: ".$rows_count."<br />";
-				?>
-				<select name="ModelPhotoRef">
-				<?php
+			<?php
+				$query=("SELECT * FROM Matchbox_References ORDER BY RefCode ASC");								
+				$result=0;
+				$rows_count=0;									
+				$result = mysql_query($query);
+				if (!result) {
+					echo "Database query failed";
+				}
+				else {
+					//echo "made connection ".$result."<br />";		
+				}
+				$rows_count= mysql_num_rows($result);
+				// echo "Rows Count: ".$rows_count."<br />";
+			?>
+			<select name="ModelPhotoRef" id="ModelPhotoRef">
+			<?php
 					for ($i=1; $i<=$rows_count; $i++) {
-						$row=mysql_fetch_array($result);
-						echo '<option value="'.$row["RefCode"].'">'.$row["RefCode"].'</option'."<br />";
-					}	
-				?>
-				</select>
+					$row=mysql_fetch_array($result);
+					echo '<option value="'.$row["RefCode"].'">'.$row["RefCode"].'</option>';
+				}	
+			?>
+			</select>
+			
 			<label for="YrFirstProduced">Year First Released:</label>	
 			<input type="text" name="YrFirstProduced" value="" size="4" id="YrFirstProduced">
+			
 			<label for="VehicleType">Vehicle Type:</label>	
-				<?php
-					$query=("SELECT * FROM Matchbox_Value_Lists WHERE ValueList LIKE '%VehicleType%'");								
-					$result=0;
-					$rows_count=0;									
-					$result = mysql_query($query);
-					if (!result) {
-						echo "Database query failed";
-					}
-					else {
-						//echo "made connection ".$result."<br />";		
-					}
-					$rows_count= mysql_num_rows($result);
-					// echo "Rows Count: ".$rows_count."<br />";
-				?>
-				<select name="VehicleType">
-				<?php
-					for ($i=1; $i<=$rows_count; $i++) {
-						$row=mysql_fetch_array($result);
-						echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option'."<br />";
-					}	
-				?>
-				</select>
+			<?php
+				$query=("SELECT * FROM Matchbox_Value_Lists WHERE ValueList LIKE '%VehicleType%'");								
+				$result=0;
+				$rows_count=0;									
+				$result = mysql_query($query);
+				if (!result) {
+					echo "Database query failed";
+				}
+				else {
+					//echo "made connection ".$result."<br />";		
+				}
+				$rows_count= mysql_num_rows($result);
+				// echo "Rows Count: ".$rows_count."<br />";
+			?>
+			<select name="VehicleType" id="VehicleType">
+			<?php
+				for ($i=1; $i<=$rows_count; $i++) {
+					$row=mysql_fetch_array($result);
+					echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option>';
+				}	
+			?>
+			</select>
 			<label for="VehicleType2">Additional Vehicle Type:</label>
-				<?php
-					$query=("SELECT * FROM Matchbox_Value_Lists WHERE ValueList LIKE '%VehicleType%'");						
-					$result=0;
-					$rows_count=0;									
-					$result = mysql_query($query);
-					if (!result) {
-						echo "Database query failed";
-					}
-					else {
-						//echo "made connection ".$result."<br />";		
-					}
-					$rows_count= mysql_num_rows($result);
-					// echo "Rows Count: ".$rows_count."<br />";
-				?>
-				<select name="VehicleType2">
-				<?php
-					for ($i=1; $i<=$rows_count; $i++) {
-						$row=mysql_fetch_array($result);
-						echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option'."<br />";
-					}	
-				?>
-				</select>
+			<?php
+				$query=("SELECT * FROM Matchbox_Value_Lists WHERE ValueList LIKE '%VehicleType%'");						
+				$result=0;
+				$rows_count=0;									
+				$result = mysql_query($query);
+				if (!result) {
+					echo "Database query failed";
+				}
+				else {
+					//echo "made connection ".$result."<br />";		
+				}
+				$rows_count= mysql_num_rows($result);
+				// echo "Rows Count: ".$rows_count."<br />";
+			?>
+			<select name="VehicleType2" id="VehicleType2">
+			<?php
+				for ($i=1; $i<=$rows_count; $i++) {
+					$row=mysql_fetch_array($result);
+					echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option>';
+				}	
+			?>
+			</select>
+			
 			<label for="ModelScale">Scale:</label>
 			<input type="text" name="ModelScale" value="" size="10" id="ModelScale">
+			
 			<label for="BaseCastYr">Base Casting Date:</label>	
 			<input type="text" name="BaseCastYr" value="" size="10" id="BaseCastYr">
+			
 			<label for="ModelComment">Model Comment:</label>
-				<textarea name="ModelComment" cols="45" rows="4">	
-				</textarea>
+			<textarea name="ModelComment" id="ModelComment" cols="45" rows="4"></textarea>
+			
 			<label for="SimilarModels">Similar Models:</label>	
-				<textarea name="SimilarModels" cols="45" rows="4">			
-				</textarea>
+			<textarea name="SimilarModels" id="SimilarModels" cols="45" rows="4"></textarea>
+			
 			<label for="MakeofModel">Make of Model:</label>	
-				<?php
-					$query=("SELECT * FROM Matchbox_Value_lists WHERE ValueList LIKE '%VehMake%'");								
-					$result=0;
-					$rows_count=0;									
-					$result = mysql_query($query);
-					if (!$result) {
-						echo "Database query failed";
-					}
-					else {
-						//echo "made connection ".$result."<br />";		
-					}
-					$rows_count= mysql_num_rows($result);
-					// echo "Rows Count: ".$rows_count."<br />";
-				?>
-				<select name="MakeofModel">
-				<?php
-					for ($i=1; $i<=$rows_count; $i++) {
-						$row=mysql_fetch_array($result);
-						echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option'."<br />";
-					}	
-				?>
-				</select>
+			<?php
+				$query=("SELECT * FROM Matchbox_Value_lists WHERE ValueList LIKE '%VehMake%'");								
+				$result=0;
+				$rows_count=0;									
+				$result = mysql_query($query);
+				if (!$result) {
+					echo "Database query failed";
+				}
+				else {
+					//echo "made connection ".$result."<br />";		
+				}
+				$rows_count= mysql_num_rows($result);
+				// echo "Rows Count: ".$rows_count."<br />";
+			?>
+			<select name="MakeofModel" id="MakeofModel">
+			<?php
+				for ($i=1; $i<=$rows_count; $i++) {
+					$row=mysql_fetch_array($result);
+					echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option>';
+				}	
+			?>
+			</select>
+			
 			<label for="CountryofMake">Country of Make:</label>		
-				<?php
-					$query=("SELECT * FROM Matchbox_Value_lists WHERE ValueList LIKE '%MakeCountry%'");								
-					$result=0;
-					$rows_count=0;									
-					$result = mysql_query($query);
-					if (!$result) {
-						echo "Database query failed";
-					}
-					else {
-						//echo "made connection ".$result."<br />";		
-					}
-					$rows_count= mysql_num_rows($result);
-					// echo "Rows Count: ".$rows_count."<br />";
-				?>
-				<select name="CountryofMake">
-				<?php
-					for ($i=1; $i<=$rows_count; $i++) {
-						$row=mysql_fetch_array($result);
-						echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option'."<br />";
-					}	
-				?>
-				</select>
-			<!-- Search Submit -->	
-			<input type="submit" class="button dark" value="Submit"/>
-		</form>			
-		<a href="Add_Model_Form.php">Cancel / Clear</a>						 						
+			<?php
+				$query=("SELECT * FROM Matchbox_Value_lists WHERE ValueList LIKE '%MakeCountry%'");								
+				$result=0;
+				$rows_count=0;									
+				$result = mysql_query($query);
+				if (!$result) {
+					echo "Database query failed";
+				}
+				else {
+					//echo "made connection ".$result."<br />";		
+				}
+				$rows_count= mysql_num_rows($result);
+				// echo "Rows Count: ".$rows_count."<br />";
+			?>
+			<select name="CountryofMake" id="CountryofMake">
+			<?php
+				for ($i=1; $i<=$rows_count; $i++) {
+					$row=mysql_fetch_array($result);
+					echo '<option value="'.$row["ValueListEntry"].'">'.$row["ValueListEntry"].'</option'."<br />";
+				}	
+			?>
+			</select>
+			
+			<div class="row">
+				<div class="large-3 small-6 columns">
+					<input type="submit" class="button dark" value="Submit"/>
+				</div>
+				<div class="large-3 small-6 columns end">
+					<a class="button dark cancel" href="Add_Model_Form.php">Cancel / Clear</a>
+				</div>
+			</div>	
+		</form>						 						
 	</div>
 </div>
 
