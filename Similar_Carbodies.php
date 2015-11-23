72e9cd62009af43ecffbb09955147a110baf2d12
Duncan Falk <djf@duncanfalk.com>
2015-11-23 09:42:57 -0500

    Formatting: Similar Carbodies

--- Similar_Carbodies.php
+++ Similar_Carbodies.php
@@ -1,39 +1,46 @@
 <?php include("includes/header.php"); ?>
-<table id="structure">
-	<tr>
-		<td id="navigation">
-			<a href="index.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Main Page</p></a>
-			<a href="Search_Models_Menu.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Search Models</p></a>
-		</td>
-		<td id="page">
-			<h2>Differences Between Superfast Carbodies Taxi Models</h2>
-			<p>The Carbodies Taxi FX 4 R was released in 1987 as MB174 (UMID SF0322). A handful of models were made, mostly released in Britain, Australia or ROW. An updated version called the Carbodies London Taxi was issued in 2005 as MB667 (UMID SF0714), with releases in the 1-75, Superfast and other series. Although no official blank was made, several Code 2 models were created against various of the standard versions.  </p>
-			<p>The base information and casting are very different:</>
-                        <p>1)  MB174 is called Taxi FX4R; MB667 is London taxi</p>
-			<p>2)  The base dates reflect their release years</p>
-			<p>3)  The scale for MB174 is 1:60; for MB667 it's 1:63</p>
-			<p>4)  The undercarriage casting fo MB174 is simpler, with an 'open pliers' appearance and a single exhaust pipe to the rear. Dual exhaust pipes on the newer issue.</p>
-			<img src="images/similar_models/MAN-174-2.jpg" width=340>
-			<img src="images/similar_models/MAN-667-2.jpg" width=340>	
-			<p></p>
-                        <p>Other differentiating features are:</p> 
-                        <p>1)  The rear door on MB174 opens; it is cast shut on MB667<p>
-                        <p>2)  There is a rectangular license plate cast into the front bumper of MB667; no plate on the smooth bumper for MB174.</p>
-			<p>3)  A small sidelight is cast onto both front fenders of MB667; MB174 fenders are smooth.</p>
-			<p>4) Metal base on MB174, plastic for MB667.</p>
 
-			MB174:
-			<br></>
- 			<img src="images/similar_models/MAN-174.jpg" width=340>
-                       
-			<br></>	
-                        MB667:
-			<br></>
-			<img src="images/similar_models/MAN-667.jpg" width=340>
-                        
-			<p></p>
+<div class="row">
+	<div class="large-12 columns">
+		<h2>Differences Between Superfast Carbodies Taxi Models</h2>
+		<p>The Carbodies Taxi FX 4 R was released in 1987 as MB174 (UMID SF0322). A handful of models were made, mostly released in Britain, Australia or ROW. An updated version called the Carbodies London Taxi was issued in 2005 as MB667 (UMID SF0714), with releases in the 1-75, Superfast and other series. Although no official blank was made, several Code 2 models were created against various of the standard versions.</p>
+		<p>The base information and casting are very different:</p>
+		<ol>
+			<li>MB174 is called Taxi FX4R; MB667 is London taxi</li>
+			<li>The base dates reflect their release years</li>
+			<li>The scale for MB174 is 1:60; for MB667 it's 1:63</li>
+			<li>The undercarriage casting fo MB174 is simpler, with an 'open pliers' appearance and a single exhaust pipe to the rear. Dual exhaust pipes on the newer issue.</li>
+		</ol>
+		<ul class="large-block-grid-2 small-block-grid-2">
+			<li><img src="images/similar_models/MAN-174-2.jpg" /></li>
+			<li><img src="images/similar_models/MAN-667-2.jpg" /></li>
+		</ul>
+		
+		<p>Other differentiating features are:</p>
+		<ol>
+			<li>The rear door on MB174 opens; it is cast shut on MB667</li>
+			<li>There is a rectangular license plate cast into the front bumper of MB667; no plate on the smooth bumper for MB174.</li>
+			<li>A small sidelight is cast onto both front fenders of MB667; MB174 fenders are smooth.</li>
+			<li>Metal base on MB174, plastic for MB667.</li>
+		</ol>
+		<hr />
+		
+		<h4>MB174:</h4>
+		<img src="images/similar_models/MAN-174.jpg" />
+		<hr />
+		
+		<h4>MB667:</h4>
+		<img src="images/similar_models/MAN-667.jpg" />
+	</div>
+</div>		
 
-		</td>
-	</tr>
-</table>
+<!-- Sub Menu -->
+<div class="row" id="subNav">
+	<div class="large-12 columns">
+		<p class="tip">related pages:</p>
+		<a href="index.php">Main Page</a>
+		<a href="Search_Models_Menu.php">Search Models</a>
+	</div>
+</div>
+
 <?php include("includes/footer.php"); ?>
