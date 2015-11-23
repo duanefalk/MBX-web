4cf6c5f1f000a2c17ed7149a067067eaab6de86e
Duncan Falk <djf@duncanfalk.com>
2015-11-23 09:33:40 -0500

    Formatting: Similar Luminas

--- Similar_Luminas.php
+++ Similar_Luminas.php
@@ -1,62 +1,70 @@
 <?php include("includes/header.php"); ?>
-<table id="structure">
-	<tr>
-		<td id="navigation">
-			<a href="index.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Main Page</p></a>
-			<a href="Search_Models_Menu.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Search Models</p></a>
-		</td>
-		<td id="page">
-			<h2>Differences Among NASCAR Lumina Models</h2>
-			<p>Matchbox released 3 Chevrolet Lumina models. In 1990 NASCAR was suddenly popular with the success of the movie &#34;Days of Thunder&#34; and the diecast company Racing Champions got its start making models from the movie. Matchbox wanted to get into the game as well and initially used some of the Racing Champion models, released under the Matchbox name. These are not Matchbox models though (they say Racing Champions on the base) and are not listed in the database, though there is a note regarding them in the SF0362 model record.</p></p>
-                        <p>The model is distinctive- actually a poor quality mold- and needs no detail listing to tell it apart:</p>
-			<img src="images/similar_models/RC Lumina_1.jpg" width=200>
-			<img src="images/similar_models/RC Lumina_2.jpg" width=200>
-			<p></p>  
-			<p>MB224 / SF0360<p>
 
-			<p>The first Matchbox-made model to be called a Lumina was a modified version of the Buick LeSabre, used in lieu of the later MB221, which was not ready in time for release.</p>
-                        <p>Differentiating points from later Luminas: </p>
-			<ol>
-				<li>On the front end, the grill space is very flat. Other models have a curved or triangular shape</li>
-				<li>Also on the front, Lumina is inscribed, as it is with MB221 (SF0360), but the groove directly above it is interrupted with 2 thin rectangular pockets.</li>
-				<li>The side of the car is sculpted with a sleeker look than the later Luminas</li>
-				<li>The side windows are shaped differently. The front window is narrower and the back is longer (left to right) than other models.</li>
-				<li>The base is similar to MAN221, with 4 engine pipes and the Matchbox logo printed normally (not compressed as in MB267)</li>
-			</ol>
- 			<img src="images/similar_models/MAN 224_1.jpg" width=340>
-                        <img src="images/similar_models/MAN 224_2.jpg" width=340>
-                        <img src="images/similar_models/MAN 224_3.jpg" width=340>
-			<p></p>	
+<div class="row">
+	<div class="large-12 columns">
+		<h2>Differences Among NASCAR Lumina Models</h2>
+		<p>Matchbox released 3 Chevrolet Lumina models. In 1990 NASCAR was suddenly popular with the success of the movie &#34;Days of Thunder&#34; and the diecast company Racing Champions got its start making models from the movie. Matchbox wanted to get into the game as well and initially used some of the Racing Champion models, released under the Matchbox name. These are not Matchbox models though (they say Racing Champions on the base) and are not listed in the database, though there is a note regarding them in the SF0362 model record.</p>
+		<p>The model is distinctive- actually a poor quality mold- and needs no detail listing to tell it apart:</p>
+		<ul class="large-block-grid-2 small-block-grid-2">
+			<li><img src="images/similar_models/RC Lumina_1.jpg"></li>
+			<li><img src="images/similar_models/RC Lumina_2.jpg"></li>
+		</ul>
+		<hr />
+	
+		<h4>MB224 / SF0360</h4>
+		<p>The first Matchbox-made model to be called a Lumina was a modified version of the Buick LeSabre, used in lieu of the later MB221, which was not ready in time for release.</p>
+		<p>Differentiating points from later Luminas:</p>
+		<ol>
+			<li>On the front end, the grill space is very flat. Other models have a curved or triangular shape</li>
+			<li>Also on the front, Lumina is inscribed, as it is with MB221 (SF0360), but the groove directly above it is interrupted with 2 thin rectangular pockets.</li>
+			<li>The side of the car is sculpted with a sleeker look than the later Luminas</li>
+			<li>The side windows are shaped differently. The front window is narrower and the back is longer (left to right) than other models.</li>
+			<li>The base is similar to MAN221, with 4 engine pipes and the Matchbox logo printed normally (not compressed as in MB267)</li>
+		</ol>
+		<ul class="large-block-grid-3 small-block-grid-3">
+			<li><img src="images/similar_models/MAN 224_1.jpg"></li>
+			<li><img src="images/similar_models/MAN 224_2.jpg"></li>
+			<li><img src="images/similar_models/MAN 224_3.jpg"></li>
+		</ul>
+		<hr />
+ 		
+ 		<h4>MB221 / SF0362</h4>
+ 		<p>The First real Matchbox model was MB221 (SF0362). The first releases, in 1990, replaced MB224 as &#34;DOT&#34; cars, but  later that year they also incorporated the Lumina stock car into the 1-75 series. This mold was adopted in 1991 for White Rose Collectibles NASCAR Chevy releases, and used  by them through 1993. A white blank of the model was made and has been very popular for Code 2 models,  even up to today. This model, and the one released in 1994, are similar and the hardest to tell apart.</p>
+		<ol>
+			<li>Lumina is inscribed in the front flange. As well, there are 2 rectangular indentations to either side, and one higher up, to the right of the grill</li>
+			<li>This model has 3 distinct grooves in the front end, one large on bottom, and two thinner above</li>
+			<li>The bottom of the rear spoiler has a triangular shape</li>
+			<li>Base details are very different from MB267 (i.e. 4 pipes coming out of the engine base). Particularly- note that the Matchbox logo is a normal shape and is located mid-right of the base. This isn&#39;t the case, below with the later model. Note- this base is very similar to MB224 (SF0362)</li>
+		</ol>
+		<ul class="large-block-grid-3 small-block-grid-3">
+			<li><img src="images/similar_models/MAN 221_1.jpg"></li>
+			<li><img src="images/similar_models/MAN 221_2.jpg"></li>
+			<li><img src="images/similar_models/MAN 221_3.jpg"></li>
+		</ul>
+		<hr />
 
-			
-			<p>MB221 / SF0362</p>
-			<p>The First real Matchbox model was MB221 (SF0362). The first releases, in 1990, replaced MB224 as &#34;DOT&#34; cars, but  later that year they also incorporated the Lumina stock car into the 1-75 series. This mold was adopted in 1991 for White Rose Collectibles NASCAR Chevy releases, and used  by them through 1993. A white blank of the model was made and has been very popular for Code 2 models,  even up to today. This model, and the one released in 1994, are similar and the hardest to tell apart.</p>
-			<ol>
-				<li>Lumina is inscribed in the front flange. As well, there are 2 rectangular indentations to either side, and one higher up, to the right of the grill</li>
-				<li>This model has 3 distinct grooves in the front end, one large on bottom, and two thinner above</li>
-				<li>The bottom of the rear spoiler has a triangular shape</li>
-				<li>Base details are very different from MB267 (i.e. 4 pipes coming out of the engine base). Particularly- note that the Matchbox logo is a normal shape and is located mid-right of the base. This isn&#39;t the case, below with the later model. Note- this base is very similar to MB224 (SF0362)</li>
-			</ol>
-
- 			<img src="images/similar_models/MAN 221_1.jpg" width=340>
-                        <img src="images/similar_models/MAN 221_2.jpg" width=340>
-                        <img src="images/similar_models/MAN 221_3.jpg" width=340>			
-			<p></p>
-			<p>MB267 (SF0396)</p>
-			<ol>
-				<li>No Lumina inscribed in the front flange. The bottom flange is completely flat, with no rectangular indentations</li>
-				<li>There is only one groove in the front</li>
-				<li>The bottom of the rear spoiler has a more rounded contour</li>
-				<li>Base details differ considerable. The Matchbox logo has a squashed (left to right) look, and is located in the upper left part of the base</li>
-			</ol>
-			<p></p>
-			                      
- 			<img src="images/similar_models/MAN 267_1.jpg" width=340>
-			<img src="images/similar_models/MAN 267_2.jpg" width=340>
-			<img src="images/similar_models/MAN 267_3.jpg" width=340>
-			<p></p>
-
-		</td>
-	</tr>
-</table>
+		<h4>MB267 (SF0396)</h4>
+		<ol>
+			<li>No Lumina inscribed in the front flange. The bottom flange is completely flat, with no rectangular indentations</li>
+			<li>There is only one groove in the front</li>
+			<li>The bottom of the rear spoiler has a more rounded contour</li>
+			<li>Base details differ considerable. The Matchbox logo has a squashed (left to right) look, and is located in the upper left part of the base</li>
+		</ol>
+		<ul class="large-block-grid-3 small-block-grid-3">
+			<li><img src="images/similar_models/MAN 267_1.jpg"></li>
+			<li><img src="images/similar_models/MAN 267_2.jpg"></li>
+			<li><img src="images/similar_models/MAN 267_3.jpg"></li>
+		</ul>
+	</div>
+</div>
+		
+<!-- Sub Menu -->
+<div class="row" id="subNav">
+	<div class="large-12 columns">
+		<p class="tip">related pages:</p>
+		<a href="index.php">Main Page</a>
+		<a href="Search_Models_Menu.php">Search Models</a>
+	</div>
+</div>
+		
 <?php include("includes/footer.php"); ?>
