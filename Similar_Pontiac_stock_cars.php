1bf43528424a68d95212034e6f040999dc4e39d8
Duncan Falk <djf@duncanfalk.com>
2015-11-25 10:13:04 -0500

    Formatting: Similar Pontiac Stock Cars

--- Similar_Pontiac_stock_cars.php
+++ Similar_Pontiac_stock_cars.php
@@ -1,71 +1,78 @@
 <?php include("includes/header.php"); ?>
-<table id="structure">
-	<tr>
-		<td id="navigation">
-			<a href="index.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Main Page</p></a>
-			<a href="Search_Models_Menu.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Search Models</p></a>
-		</td>
-		<td id="page">
-			<h2>Differences Among NASCAR Pontiac Models</h2>
-			<p>Matchbox released 4 Pontiac Grand Prix models:</p>
-                      
-			<p></p>	
-			<p>MB216 / SF0370
-                        <ol>
-				<li>2 grill openings, each are 5-sided shape, closest sides are parallel</li>
-				<li>Several grooves two each side of grill; top groove even with top of grill</li>
-				<li>3 front window braces, 3 rear</li>
-				<li>flat spoiler</li>
-				<li>1991 Pontiac Grand Prix on base</li>
-			</ol>
- 			<img src="images/similar_models/MAN 216_1.jpg" width=340>
-                        <img src="images/similar_models/MAN 216_2.jpg" width=340>
-                        <img src="images/similar_models/MAN 216_3.jpg" width=340>
-			<p></p>	
 
-			
-			<p>MB238 / SF0377</p>
-                        <ol>
-				<li>2 grill openings, each are 5-sided shape, closest sides are parallel</li>
-				<li>no grooves (or very faint grooves) to sides of grill</li>
-				<li>3 front window braces, 3 rear</li>
-				<li>flat spoiler with line of rivets under it</li>
-				<li>1992 Grand Prix Stock Car</li>
-			</ol>
+<div class="row">
+	<div class="large-12 columns">
+		<h2>Differences Among NASCAR Pontiac Models</h2>
+		<p>Matchbox released 4 Pontiac Grand Prix models:</p>
+        <h4>MB216 / SF0370</h4>
+        <ol>
+			<li>2 grill openings, each are 5-sided shape, closest sides are parallel</li>
+			<li>Several grooves two each side of grill; top groove even with top of grill</li>
+			<li>3 front window braces, 3 rear</li>
+			<li>flat spoiler</li>
+			<li>1991 Pontiac Grand Prix on base</li>
+		</ol>
+		<ul class="large-block-grid-3 small-block-grid-3">
+			<li><img src="images/similar_models/MAN 216_1.jpg" /></li>
+			<li><img src="images/similar_models/MAN 216_2.jpg" /></li>
+			<li><img src="images/similar_models/MAN 216_3.jpg" /></li>
+		</ul>
+		<hr />
+		
+		<h4>MB238 / SF0377</h4>
+		<ol>
+			<li>2 grill openings, each are 5-sided shape, closest sides are parallel</li>
+			<li>no grooves (or very faint grooves) to sides of grill</li>
+			<li>3 front window braces, 3 rear</li>
+			<li>flat spoiler with line of rivets under it</li>
+			<li>1992 Grand Prix Stock Car</li>
+		</ol>
+		<ul class="large-block-grid-3 small-block-grid-3">
+			<li><img src="images/similar_models/MAN 238_1.jpg" /></li>
+			<li><img src="images/similar_models/MAN 238_2.jpg" /></li>
+			<li><img src="images/similar_models/MAN 238_3.jpg" /></li>
+		</ul>
+		<hr />
+		
+		<h4>MB269 / SF0398</h4>
+        <ol>
+			<li>2 grill openings, each are 5-sided shape, closest sides are parallel</li>
+			<li>Several grooves to each side of grill; top groove is above grill and goes all across the front</li>
+			<li>4 front window braces, 2 rear window braces</li>
+			<li>flat spoiler</li>
+			<li>1993 Grand Prix Pontiac</li>
+		</ol>
+		<ul class="large-block-grid-3 small-block-grid-3">
+			<li><img src="images/similar_models/MAN 269_1.jpg" /></li>
+			<li><img src="images/similar_models/MAN 269_2.jpg" /></li>
+			<li><img src="images/similar_models/MAN 269_3.jpg" /></li>
+		</ul>
+		<hr />
+		
+		<h4>WRP01 / SF0427</h4>
+		<ol>
+			<li>2 grill openings, meet at bottom, 4-sided</li>
+			<li>indented &#39;shelf&#39; beside grills</li>
+			<li>3 front window braces, 3 rear</li>
+			<li>flat spoiler</li>
+			<li>1996 Grand Prix Pontiac</li>
+		</ol>
+		<ul class="large-block-grid-3 small-block-grid-3">
+			<li><img src="images/similar_models/MAN WRP01_1.jpg" /></li>
+			<li><img src="images/similar_models/MAN WRP01_2.jpg" /></li>
+			<li><img src="images/similar_models/MAN WRP01_3.jpg" /></li>
+		</ul>
+		<hr />
+	</div>
+</div>
 
- 			<img src="images/similar_models/MAN 238_1.jpg" width=340>
-                        <img src="images/similar_models/MAN 238_2.jpg" width=340>
-                        <img src="images/similar_models/MAN 238_3.jpg" width=340>			
-			<p></p>
-			
-			<p>MB269 / SF0398</p>
-	                <ol>
-				<li>2 grill openings, each are 5-sided shape, closest sides are parallel</li>
-				<li>Several grooves to each side of grill; top groove is above grill and goes all across the front</li>
-				<li>4 front window braces, 2 rear window braces</li>
-				<li>flat spoiler</li>
-				<li>1993 Grand Prix Pontiac</li>
-			</ol>
-			<p></p>
-			                      
- 			<img src="images/similar_models/MAN 269_1.jpg" width=340>
-			<img src="images/similar_models/MAN 269_2.jpg" width=340>
-			<img src="images/similar_models/MAN 269_3.jpg" width=340>
-			<p></p>
+<!-- Sub Menu -->
+<div class="row" id="subNav">
+	<div class="large-12 columns">
+		<p class="tip">related pages:</p>
+		<a href="index.php">Main Page</a>
+		<a href="Search_Models_Menu.php">Search Models</a>
+	</div>
+</div>
 
-			<p>WRP01 / SF0427</p>
-			<ol>
-				<li>2 grill openings, meet at bottom, 4-sided</li>
-				<li>indented &#39;shelf&#39; beside grills</li>
-				<li>3 front window braces, 3 rear</li>
-				<li>flat spoiler</li>
-				<li>1996 Grand Prix Pontiac</li>
-			</ol>
-			<img src="images/similar_models/MAN WRP01_1.jpg" width=340>
-			<img src="images/similar_models/MAN WRP01_2.jpg" width=340>
-			<img src="images/similar_models/MAN WRP01_3.jpg" width=340>
-			<p></p>
-		</td>
-	</tr>
-</table>
 <?php include("includes/footer.php"); ?>
