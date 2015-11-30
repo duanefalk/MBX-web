97110ada25cd802037272e7765f13cd16fe55740
Duncan Falk <djf@duncanfalk.com>
2015-11-30 13:48:08 -0500

    Formatting: Similar Z3s

--- Similar_Z3s.php
+++ Similar_Z3s.php
@@ -1,33 +1,41 @@
 <?php include("includes/header.php"); ?>
-<table id="structure">
-	<tr>
-		<td id="navigation">
-			<a href="index.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Main Page</p></a>
-			<a href="Search_Models_Menu.php"><p onmouseover="this.style.color='orange'" onmouseout="this.style.color='white'">Return to Search Models</p></a>
-		</td>
-		<td id="page">
-			<h2>Differences Between Superfast BMW Z3 Models</h2>
-			<p>The 1939 Chevy Sedan Delivery was originally commissioned by White Rose. The initial model (MB215) was too small for their requirements though, and a 2nd version (MB245) was created at a larger scale and released by that company. The original version was later used by Matchbox for several models and released as a blank for Code 2's. </p>
-			<p>The easiest way to distinguish them, if you have access to the model, is to check the scale on the base. MB215 is at 1:63 scale while MB245 is 1:57. These are clearly stamped on the base.</>
-                        <p>MB215 & MB245 sizes (thx to C Falkensteiner for comparison image):</p>
-			<img src="images/similar_models/MI215-MI245.jpg" width=340>
-			<p></p>
-                        <p>Other differentiating features are:</p> 
-                        <p>1)  MB215 has a distinct rear bumper; MB245 has no rear bumper</p>
-                        <p>2)  Axle covers at the wheels are cast differently. MB215 has plastic covers cast to the wheels themselves. MB245 has covers cast as par tof the base. </p>
 
-			MB215:
-			<br></>
- 			<img src="images/similar_models/MAN-215.jpg" width=340>
-                        <img src="images/similar_models/MAN-215-2.jpg" width=340>
-			<br></>	
-                        MB245:
-			<br></>
-			<img src="images/similar_models/MAN-245.jpg" width=340>
-                        <img src="images/similar_models/MAN-245-2.jpg" width=340>
-			<p></p>
+<div class="row">
+	<div class="large-12 columns">		
+		<h2>Differences Between Superfast BMW Z3 Models</h2>
+		<p>The 1939 Chevy Sedan Delivery was originally commissioned by White Rose. The initial model (MB215) was too small for their requirements though, and a 2nd version (MB245) was created at a larger scale and released by that company. The original version was later used by Matchbox for several models and released as a blank for Code 2's.</p>
+		<p>The easiest way to distinguish them, if you have access to the model, is to check the scale on the base. MB215 is at 1:63 scale while MB245 is 1:57. These are clearly stamped on the base.</p>
+		<p>MB215 & MB245 sizes (thanks to C Falkensteiner for comparison image):</p>
+		<img src="images/similar_models/MI215-MI245.jpg" />
+		<p>Other differentiating features are:</p> 
+		<ol>
+			<li>MB215 has a distinct rear bumper; MB245 has no rear bumper</li>
+			<li>Axle covers at the wheels are cast differently. MB215 has plastic covers cast to the wheels themselves. MB245 has covers cast as part of the base.</li>
+		</ol>
+		<hr />
+		
+		<h4>MB215:</h4>		
+		<ul class="large-block-grid-3 small-block-grid-3">
+			<li><img src="images/similar_models/MAN-215.jpg" /></li>
+			<li><img src="images/similar_models/MAN-215-2.jpg" /></li>
+		</ul>
+		<hr />
+		
+		<h4>MB245:</h4>
+		<ul class="large-block-grid-3 small-block-grid-3">
+			<li><img src="images/similar_models/MAN-245.jpg" /></li>
+			<li><img src="images/similar_models/MAN-245-2.jpg" /></li>
+		</ul>
+	</div>
+</div>
 
-		</td>
-	</tr>
-</table>
+<!-- Sub Menu -->
+<div class="row" id="subNav">
+	<div class="large-12 columns">
+		<p class="tip">related pages:</p>
+		<a href="index.php">Main Page</a>
+		<a href="Search_Models_Menu.php">Search Models</a>
+	</div>
+</div>
+
 <?php include("includes/footer.php"); ?>
