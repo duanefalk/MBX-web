3c1215f994a46c1048c0e5d29bc412373398fcf8
Duncan Falk <djf@duncanfalk.com>
2016-03-28 15:55:16 -0400

    Navigation Menu: moving Website Updates
    Website Updates is now visible to all visitors, logged in or not.

--- includes/navUtility.php
+++ includes/navUtility.php
@@ -13,14 +13,15 @@
 				<?php if ($_SESSION['Sec_Lvl'] == 3) { //code 3 = Dad ?>
 					<a href='Add_Menu.php'>Add a New Record</a>
 				<?php } ?>
-				<a href='website-updates.php'>Website Updates</a>
 				<a href='Edit_Account_Form.php'>Edit Account</a>
 				<a href='logout.php'>Logout</a>
 			<?php } else if ($_SESSION['Sec_Lvl'] == 1) { ?>
+				<a href='website-updates.php'>Website Updates</a>
 				<a href='Verification.php'>Create an Account</a>
 				<a href='Authenticate-test.php'>Go to Login</a>
 			<?php }	else { ?>
-				<a href='Create_User_Account_Form.php'>Create an Account</a>
+				<a href='website-updates.php'>Website Updates</a>
+				<a href='Verification.php'>Create an Account</a>
 				<a href='Authenticate-test.php'>Login</a>
 			<?php }	?>
 		</div>
