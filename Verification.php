3fee1f7a9cec93c8ff4d1226e5ffdb403910af79
Duncan Falk <djf@duncanfalk.com>
2016-03-30 14:11:16 -0400

    Verification styling

--- Verification.php
+++ Verification.php
@@ -1,54 +1,52 @@
 <?php 
 	ob_start();
 	session_start();
-	$pageTitle = "Create an Account";
+	$pageTitle = "Verify Your Humanity";
 	$pageDescription = "Create your own Matchbox University account.";
 	require_once("includes/db_connection.php");
 	require_once("includes/functions.php");
 	include("includes/header.php");
 ?>
 
-
-<?php
-//check to see if they got the answer right; if so send to acct creation, if not exit
-    if (isset($_POST['submit'])) {
-	$VerAnswer=$_POST['VerAnswer'];
-	$CorrAns=$_POST['CorrAns'];
-	$CorrAns= strtoupper($CorrAns);
-	$VerAnswer=strtoupper($VerAnswer);
-	//echo $VerAnswer . "  " . $CorrAns;
-	//exit;
-	if ($VerAnswer==$CorrAns) {
-		redirect_to("Create_User_Account_Form.php");
-	} else {
-		echo "Incorrect. Bad 'bot!!";
-		exit;
-	}
-
-    }
-?>
-
-
-
 <div class="row">
 	<div class="large-12 columns">
 
 		<h2>Verification for Account</h2>
 		
+		<?php
+			//check to see if they got the answer right; if so send to acct creation, if not exit
+		    if (isset($_POST['submit'])) {
+				$VerAnswer=$_POST['VerAnswer'];
+				$CorrAns=$_POST['CorrAns'];
+				$CorrAns= strtoupper($CorrAns);
+				$VerAnswer=strtoupper($VerAnswer);
+				//echo $VerAnswer . "  " . $CorrAns;
+				//exit;
+				
+				if ($VerAnswer==$CorrAns) {
+					redirect_to("Create_User_Account_Form.php");
+				} else {
+					echo "<h4>Incorrect. Please try again in order to advance to account creation.</h4>";
+					//exit;
+				}
+		    }
+		?>
+
+
 		<p>To verify you are not a bot, read the lines below and answer the question.</p>
 		<?php
 			//starts out with a test line in case bot just reads first pic
-			$testPic= WEB_IMAGE_PATH . "testPic.jpg";
+			$testPic = WEB_IMAGE_PATH . "testPic.jpg";
 			echo "<p>This is a test ". "<img src=" . $testPic. " width=\"200\"></p>";
 			
 			//look up all questions and display text and photo for each line
-			$query= ("SELECT * FROM Verification");
-			$result= mysql_query($query);
-			$rows= mysql_num_rows($result);
+			$query = ("SELECT * FROM Verification");
+			$result = mysql_query($query);
+			$rows = mysql_num_rows($result);
 			for ($i=1; $i<=$rows; $i++) {
 				$row =mysql_fetch_array($result);
 				$picture = WEB_IMAGE_PATH . $row["VerPic"];
-				echo "<p>".$row["VerTxt"]."<img src=" . $picture. " width=\"200\"><p>";
+				echo "<p>".$row["VerTxt"]."<img src=" . $picture. " width=\"200\"></p>";
 			}
 			//select one of the questions to ask
 			$verRecord= rand(1,$rows);
