556bb16b1e2d1a5f9740a2c1b329bf6f0df20932
duanefalk <duanejfalk@yahoo.com>
2017-06-21 12:25:24 -0400

    cosmetic fixes

--- add-variation-to-wishlist.php
+++ add-variation-to-wishlist.php
@@ -23,18 +23,19 @@
 					    $User_Coll_ID=$row3["User_Coll_ID"];
 					
 					    $VarID=$_POST["var_to_add"];
+						//updated 062117 to parse correctly for both LRxxx and SFxxxx models							
 						$LorS=substr($VarID,0,1);
 
 						if ($LorS=="L") {
 						    $VerID=substr($VarID,0,9);
 						    $UMID=substr($VerID,0,5);
-							} elseif ($LorS=="S") {
+						} elseif ($LorS=="S") {
 							$VerID=substr($VarID,0,10);
 						    $UMID=substr($VerID,0,6);
-							} else {
+						} else {
 								echo "Invalid Variation ID";
 								exit;
-							}
+						}
 
 					    $Wishlist_InactivFlg="0";
 				
