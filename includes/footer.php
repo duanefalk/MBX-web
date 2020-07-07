98ad5475e98cfa4c7bd3ba717b7133e2c611939e
Duncan <duncan@designfreund.com>
2020-07-07 09:27:28 -0400

    Fixing link typo in footer

--- includes/footer.php
+++ includes/footer.php
@@ -10,7 +10,7 @@
 		<div class="large-12 columns">
 			<p>Copyright &copy; <?php echo date("Y"); ?> Matchbox University / D. Falk, All Rights Reserved.</p>
 			<p>MBX-U.com is in no way affiliated with Mattel, Inc. The MATCHBOX&copy; name and logo are the sole properties of Mattel, Inc.</p>
-			<p><a href="<?php ROOTURL; ?>website_feedback.php">Have Feedback on the Website? Its great to hear your feedback, shoot us a message.</a></p>
+			<p><a href="<?php ROOTURL; ?>website-feedback.php">Have Feedback on the Website? Its great to hear your feedback, shoot us a message.</a></p>
 		</div>
 	</div>  
 </footer>
