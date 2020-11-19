9b188707f2aa3de14ee4b1a0eccc1d189719851b
Duncan <duncan@designfreund.com>
2020-11-18 23:14:30 -0500

    Announcement: revising announcement text

--- includes/footer.php
+++ includes/footer.php
@@ -17,10 +17,10 @@
 
 <div id="announcement-popup" class="modal">
 	<h4>ATTENTION:</h4>
-	<p>Due to a number of circumstances I will no longer be updating or maintaining the MBX-U website after the end of 2020. I will leave the site running as it stands for several months to allow folks time to do some final lookups or print/export their collections if desired.</p>
-	<p>Two things&mdash;</p>
-	<p>First, If there is anyone who is interested and has web site admin skills, I would be amenable to transferring the site (code, database, photos, domain name) to someone who could keep it going. If you are interested and able to do this, email me at <a href="mailto:info@mbx-U.com">info@mbx-u.com</a>.</p>
-	<p>Second, I will be selling my collection. If you are interested in purchasing a large collection, or even just some particular models, contact me at the above email as well with your wish-list.</p>
+	<p>We are currently working with an individual to take over and maintain the site. Please stay tuned for info on that changeover.</p>
+	<p>(Due to a number of circumstances I will no longer be updating or maintaining the MBX-U website after the end of 2020. I will leave the site running as it stands for several months to allow folks time to do some final lookups or print/export their collections if desired.)</p>
+	<p>Note &mdash;</p>
+	<p>I am selling my collection. If you are interested in purchasing a large collection, or even just some particular models, contact me at <a href="info@mbx-u.com">info@mbx-u.com</a> with your wish-list.</p>
 	<p>Thank you to everyone who's been supportive of the site!</p>
 	<p>Regards,<br>Duane</p>
 </div>
